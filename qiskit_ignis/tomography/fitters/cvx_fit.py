13f7565b4c7ccbb175f37162ccc1685d5a9d9b76
Gadi Aleksandrowicz <gadia@il.ibm.com>
2018-12-12 09:59:39 +0200

    Fix to allow cvx_fit to retry and finally gracefully fail if the solver cannot handle the problem

--- qiskit_ignis/tomography/fitters/cvx_fit.py
+++ qiskit_ignis/tomography/fitters/cvx_fit.py
@@ -234,7 +234,25 @@ def cvx_fit(data, basis_matrix, weights=None, PSD=True, trace=None,
 
     # Solve SDP
     prob = cvxpy.Problem(obj, cons)
-    prob.solve(**kwargs)
+    iters = 5000
+    max_iters = kwargs.get('max_iters', 20000)
+
+    problem_solved = False
+    while not problem_solved:
+        kwargs['max_iters'] = iters
+        prob.solve(**kwargs)
+        if prob.status in ["optimal_inaccurate", "optimal"]:
+            problem_solved = True
+        elif prob.status == "unbounded_inaccurate":
+            if iters < max_iters:
+                iters *= 2
+            else:
+                raise RuntimeError("CVX fit failed, probably not enough iterations for the solver")
+        elif prob.status in ["infeasible", "unbounded"]:
+            raise RuntimeError("CVX fit failed, problem status {} which should not happen".format(prob.status))
+        else:
+            raise RuntimeError("CVX fit failed, reason unknown")
+
     rho_fit = rho_r.value + 1j * rho_i.value
 
     return rho_fit
