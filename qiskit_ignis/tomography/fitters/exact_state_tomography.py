25fec8acd477ece5ce13f1b1749aa65d24bb75be
Gadi Aleksandrowicz <gadia@il.ibm.com>
2019-01-17 09:37:46 +0200

    Exact state tomography now separately converts to probabilities
    Signed-off-by: Gadi Aleksandrowicz <gadia@il.ibm.com>

--- qiskit_ignis/tomography/fitters/exact_state_tomography.py
+++ qiskit_ignis/tomography/fitters/exact_state_tomography.py
@@ -7,21 +7,28 @@ Z = numpy.array([[1, 0], [0, -1]])
 
 PauliBasis = {'X': X, 'Y': Y, 'Z': Z}
 
-def compute_coefficient(v, op_names, results):
+def compute_coefficient(v, op_names, probs):
     # measurements with 'Id' are redundant; we can replace with any other operator
     replacement_op_name = op_names[-1]
     measurement_name = tuple([op_names[i] if i != 0 else replacement_op_name for i in v])
-    measurement = results[measurement_name]
-    total_weighted_sum = 0
+    measurement = probs[measurement_name]
+    total_weighted_sum = 0.0
     w = [1 if i != 0 else 0 for i in v]
-    for (measurement_result_bits, measurement_result_count) in measurement.items():
+    for (measurement_result_bits, measurement_result_prob) in measurement.items():
         measurement_bits_vector = numpy.array([int(x) for x in measurement_result_bits])
         weight = (-1) ** sum(w * measurement_bits_vector)
-        total_weighted_sum += weight * measurement_result_count
+        total_weighted_sum += weight * measurement_result_prob
 
-    measurement_count = sum([m[1] for m in measurement.items()])
-    return (float(total_weighted_sum) / measurement_count)
+    return total_weighted_sum
 
+def experimental_results_to_probabilities(results):
+    probs = {}
+    for key, measurements in results.items():
+        probs[key] = {}
+        total_measurement_count = sum(measurements.values())
+        for measurement_key, measurement_count in measurements.items():
+            probs[key][measurement_key] = measurement_count / total_measurement_count
+    return probs
 
 def get_matrix(v, matrices):
     """The tensor product of the matrices indexed by v"""
@@ -42,9 +49,9 @@ def exact_state_tomography(results, ops = 'pauli'):
     op_names = ['Id'] + list(op_names)
     op_matrices = [Id] + list(op_matrices)
     n = len(op_names)
-
+    probs = experimental_results_to_probabilities(results)
     for v in itertools.product(range(n), repeat=qubits_num):
-        coeff = compute_coefficient(v, op_names, results)
+        coeff = compute_coefficient(v, op_names, probs)
         matrix = get_matrix(v, op_matrices)
         rho += coeff * matrix
 
