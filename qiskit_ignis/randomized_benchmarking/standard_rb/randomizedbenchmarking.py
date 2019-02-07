8490f9e6a2c3a3340dba6f2e4d7a11b56e398cc3
Shelly Garion <shelly@il.ibm.com>
2019-02-07 11:21:25 +0200

    copied RB files and test from ignis internal

---
+++ qiskit_ignis/randomized_benchmarking/standard_rb/randomizedbenchmarking.py
@@ -0,0 +1,231 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+"""
+Generates randomized benchmarking sequences
+"""
+
+import os
+import copy
+import numpy as np
+import verification.randomized_benchmarking.standard_rb.Clifford as clf
+import verification.randomized_benchmarking.standard_rb.clifford_utils as clutils
+import qiskit
+
+def handle_length_multiplier(length_multiplier, len_pattern):
+    '''
+    Check validity of length_multiplier.
+    In addition, transform it into a vector if it is a constant.
+    '''
+
+    if hasattr(length_multiplier, "__len__"):
+        if len(length_multiplier) != len_pattern:
+            raise ValueError("Length mulitiplier must be the same length as the pattern")
+        length_multiplier = np.array(length_multiplier)
+        if length_multiplier.dtype != 'int' or (length_multiplier < 1).any():
+            raise ValueError("Invalid length multiplier")
+    else:
+        length_multiplier = np.ones(len_pattern, dtype='int')*length_multiplier
+
+    return length_multiplier
+
+
+def check_pattern(pattern, n_qubits):
+    '''
+    Verifies that the input pattern is valid, i.e.,
+    that each qubit appears at most once
+    '''
+
+    pattern_flat = []
+    for pat in pattern:
+        pattern_flat.extend(pat)
+
+    if np.max(pattern_flat) >= n_qubits:
+        print("Invalid pattern. Qubit index in the pattern exceeds the number of qubits.")
+
+    _, uni_counts = np.unique(np.array(pattern_flat), return_counts=True)
+    if (uni_counts > 1).any():
+        raise ValueError("Invalid pattern. Duplicate qubit index.")
+
+
+def set_defaults_if_needed(rb_opts_dict):
+    '''
+    Set default values to fields of rb_opts_dict
+    and perform validity checks
+    '''
+
+    if rb_opts_dict is None:
+        rb_opts_dict = {}
+
+    rb_opts_dict.setdefault('nseeds', 1)
+    rb_opts_dict.setdefault('length_vector', [1, 10, 20])
+    rb_opts_dict.setdefault('n_qubits', 1)
+    rb_opts_dict.setdefault('rb_pattern', [[0]])
+    check_pattern(rb_opts_dict['rb_pattern'], rb_opts_dict['n_qubits'])
+    rb_opts_dict.setdefault('length_multiplier', 1)
+    rb_opts_dict['length_multiplier'] = handle_length_multiplier(
+        rb_opts_dict['length_multiplier'],
+        len(rb_opts_dict['rb_pattern']))
+
+    valid_keys = ['nseeds', 'length_vector', 'n_qubits', 'rb_pattern', 'length_multiplier']
+    if not set(rb_opts_dict.keys()).issubset(valid_keys):
+        raise ValueError("Input dictionary contains an invalid field")
+
+
+def calc_xdata(length_vector, length_multiplier):
+    '''
+    Calculate the set of sequences lengths
+    '''
+
+    xdata = []
+    for mult in length_multiplier:
+        xdata.append(np.array(length_vector)*mult)
+
+    return np.array(xdata)
+
+
+def load_tables(max_nrb):
+    '''
+    Returns the needed Clifford tables
+    max_nrb is the number of qubits for the largest required table
+    '''
+
+    clifford_tables = [[] for i in range(max_nrb)]
+    for rb_num in range(max_nrb):
+        #load the clifford tables, but only if we're using that particular rb number
+        if rb_num == 0:
+            #1Q Cliffords, load table programmatically
+            clifford_tables[0] = clutils.clifford1_table()
+        elif rb_num == 1:
+            #2Q Cliffords
+            #Try to load the table in from file. If it doesn't exist then make the file
+            try:
+                clifford_tables[rb_num] = clutils.load_clifford_table(
+                    picklefile='cliffords%d.pickle'%(rb_num+1))
+            except OSError:
+                #table doesn't exist, so save it
+                #this will save time next run
+                print('Making the n=%d Clifford Table'%(rb_num+1))
+                clutils.pickle_clifford_table(picklefile='cliffords%d.pickle'%(rb_num+1),
+                                              n=(rb_num+1))
+                clifford_tables[rb_num] = \
+                    clutils.load_clifford_table(\
+                    picklefile='cliffords%d.pickle'%(rb_num+1))
+        elif rb_num in (2, 3):
+            if not os.path.exists(os.path.join(os.path.dirname(__file__), 'src',
+                                               'qubits_%d_cnots_0.dat'%(rb_num+1))):
+                raise ValueError("Please run 'ex %d' to generate the .dat tables"%(rb_num+1))
+
+    return clifford_tables
+
+
+#get generic randomized benchmarking sequences
+def randomized_benchmarking_seq(rb_opts_dict=None):
+    """Get a generic randomized benchmarking sequence
+    rb_opts_dict: A dictionary of RB options
+        nseeds: number of seeds
+        length_vector: 'm' length vector of Clifford lengths. Must be in ascending order.
+        RB sequences of increasing length grow on top of the previous sequences.
+        n_qubits: total number of qubits
+        rb_pattern: A list of the form [[i,j],[k],...] which will make
+        simultaneous RB sequences where
+        Qi,Qj are a 2Q RB sequence and Qk is a 1Q sequence, etc.
+        E.g. [[0,3],[2],[1]] would create RB sequences that are 2Q for Q0/Q3, 1Q for Q1+Q2
+        The number of qubits is the sum of the entries. For 'regular' RB the qubit_pattern is just
+        [[0]],[[0,1]]
+        length_multiplier: if this is an array it scales each rb_sequence by the multiplier
+
+    Returns:
+        rb_circs: list of circuits for the rb sequences
+        xdata: the Clifford lengths (with multiplier if applicable)
+        rb_opts_dict: option dictionary back out with default options appended
+    """
+
+    config = copy.deepcopy(rb_opts_dict)
+    set_defaults_if_needed(config)
+
+    rb_pattern = config['rb_pattern']
+    length_vector = config['length_vector']
+    nseeds = config['nseeds']
+    n_qubits = config['n_qubits']
+    length_multiplier = config['length_multiplier']
+
+    xdata = calc_xdata(length_vector, length_multiplier)
+    pattern_sizes = [len(pat) for pat in rb_pattern]
+    clifford_tables = load_tables(np.max(pattern_sizes))
+
+    circuits = []
+    #go through for each seed
+    for seed in range(nseeds):
+
+        qr = qiskit.QuantumRegister(n_qubits, 'qr')
+        cr = qiskit.ClassicalRegister(n_qubits, 'cr')
+        general_circ = qiskit.QuantumCircuit(qr, cr)
+
+        #make Clifford sequences for each of the separate sequences in rb_pattern
+        Cliffs = []
+
+        for rb_q_num in pattern_sizes:
+            Cliffs.append(clf.Clifford(rb_q_num))
+
+        #go through and add Cliffords
+        length_index = 0
+        for cliff_index in range(length_vector[-1]):
+            for (rb_pattern_index, rb_q_num) in enumerate(pattern_sizes):
+
+                for _ in range(length_multiplier[rb_pattern_index]):
+                    new_cliff = clutils.random_clifford(rb_q_num)
+                    Cliffs[rb_pattern_index].compose_circuit(new_cliff)
+                    general_circ += replace_q_indices(clutils.get_quantum_circuit(new_cliff),
+                                                      rb_pattern[rb_pattern_index], qr)
+                    #add a barrier
+                    general_circ.barrier(*[qr[x] for x in rb_pattern[rb_pattern_index]])
+
+            #if the number of cliffords matches one of the sequence lengths then
+            #calculate the inverse and produce the circuit
+            if (cliff_index+1) == length_vector[length_index]:
+
+                circ = qiskit.QuantumCircuit(qr, cr)
+                circ += general_circ
+
+                for (rb_pattern_index, rb_q_num) in enumerate(pattern_sizes):
+                    inv_circuit = clutils.find_inverse_clifford_circuit(Cliffs[rb_pattern_index],
+                                                                        clifford_tables[rb_q_num-1])
+                    inv = clf.Clifford(rb_q_num)
+                    inv.circuit = inv_circuit
+                    # inv is now a Clifford object whose circuit is the correct
+                    # inverse but the tableau is just identity
+                    circ += replace_q_indices(clutils.get_quantum_circuit(inv),
+                                              rb_pattern[rb_pattern_index], qr)
+
+                circ.measure(qr, cr)
+                circ.name = 'rb_seed_' + str(seed) + '_length_' + str(length_vector[length_index])
+                circuits.append(circ)
+                length_index += 1
+
+    return circuits, xdata, config
+
+
+def replace_q_indices(circuit, q_nums, qr):
+    """Take a circuit that is ordered from 0,1,2 qubits and replace 0 with the qubit
+    label in the first index of q_nums, 1 with the second index...
+
+    circuit: circuit to operate on
+    q_nums: list of qubit indices
+
+    Returns:
+    updated circuit
+    """
+
+    new_circuit = qiskit.QuantumCircuit(qr)
+    for op in circuit.data:
+        original_qubits = op.qargs
+        new_op = copy.deepcopy(op)
+        new_op.qargs = [(qr, q_nums[x]) for x in [arg[1] for arg in original_qubits]]
+        new_circuit.data.append(new_op)
+
+    return new_circuit
---
+++ tests/test_rb.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+"""
+Run through RB for different qubit numbers to check that it's working
+and that it returns the identity
+"""
+
+import unittest
+import random
+import qiskit
+import verification.randomized_benchmarking.standard_rb.randomizedbenchmarking as rb
+
+class TestRB(unittest.TestCase):
+    """ The test class """
+
+    @staticmethod
+    def choose_length_vector():
+        '''
+        Randomize a valid field for rb_opts['length_vector']
+        This function returns a vector of length between 1 to 4
+        The first element is between 1 to 3,
+        and every subsequent element is greater than the previous one
+        with difference between 1 to 3.
+        :return: the length vector
+        '''
+
+        res = []
+        last_elem = 0
+        for _ in range(random.randint(1, 4)):
+            new_elem = last_elem + random.randint(1, 3)
+            res.append(new_elem)
+            last_elem = new_elem
+
+        return res
+
+    @staticmethod
+    def choose_pattern(pattern_type, nq):
+        '''
+        Choose a valid field for rb_opts['rb_pattern']
+        :param pattern_type: a number between 0 and 2.
+                             0 - a list of all qubits, for nq=5 it is [1, 2, 3, 4, 5]
+                             1 - a list of lists of single qubits, for nq=5
+                                 it is [[1], [2], [3], [4], [5]]
+                             2 - randomly choose a pattern which is a list of two lists,
+                                for example for nq=5 it can be [[4, 1, 2], [5, 3]]
+        :param nq: number of qubits
+        :return: the pattern or None
+                 Returns None if the pattern type is not relevant to the number of qubits,
+                 i.e,, one of two cases:
+                 pattern_type = 1 and nq = 1, which implies [[1]]
+                 pattern_type = 2 and nq <= 2: - for nq=1 this is impossible
+                                               - for nq=2 this implies [[1], [2]],
+                                                 which is already tested when pattern_type = 1
+        '''
+
+        if pattern_type == 0:
+            res = [list(range(nq))]
+        elif pattern_type == 1:
+            if nq == 1:
+                return None
+            res = [[x] for x in range(nq)]
+        else:
+            if nq <= 2:
+                return None
+            shuffled_bits = list(range(nq))
+            random.shuffle(shuffled_bits)
+            split_loc = random.randint(1, nq-1)
+            res = [shuffled_bits[:split_loc], shuffled_bits[split_loc:]]
+
+        return res
+
+    @staticmethod
+    def choose_multiplier(len_pattern):
+        '''
+        Randomize a valid field for rb_opts['length_multiplier']
+        The result can be either an integer or a vector of integers
+        If it is a vector then its length must be equal to the number of patterns
+        :param len_pattern: number of patterns
+        :return: the length multiplier
+        '''
+        is_length_multipler_constant = random.randint(0, 1)
+        if is_length_multipler_constant == 0:
+            res = random.randint(1, 3)
+        else:
+            res = []
+            for _ in range(len_pattern):
+                res.append(random.randint(1, 3))
+
+        return res
+
+    def verify_circuit(self, circ, rb_opts, vec_len, result, shots):
+        '''
+        For a single sequence, verifies that it meets the requirements:
+        - Executing it on the ground state ends up in the ground state
+        - It has the correct number of Cliffords
+        - It fulfills the pattern, as specified by rb_patterns and length_multiplier
+        :param circ: the sequence to check
+        :param rb_opts: the specification that generated the set of sequences which includes circ
+        :param vec_len: the expected length vector of circ (one of rb_opts['length_vector']
+        :param result: the output of the simulator
+                       when executing all the sequences on the ground state
+        :param shots: the number of shots in the simulator execution
+        '''
+
+        ops = circ.data
+        op_index = 0
+        for _ in range(vec_len):   # for each cycle (the sequence should consist of vec_len cycles)
+            for pat_index in range(len(rb_opts['rb_pattern'])):
+                # for each component of the pattern...
+                for _ in range(rb_opts['length_multiplier'][pat_index]):  # for each Clifford...
+                    while ops[op_index].name != 'barrier':  # for each basis gate...
+                        # Verify that the gate acts on the correct qubits
+                        # This happens if the sequence is composed of the correct sub-sequences,
+                        # as specified by vec_len and rb_opts
+                        self.assertTrue(all(x[1] in rb_opts['rb_pattern'][pat_index] \
+                                            for x in ops[op_index].qargs),
+                                        "Error: operation acts on incorrect qubits")
+                        op_index += 1
+                    op_index += 1  # increment because of the barrier gate
+
+        # check if the ground state returns
+        self.assertEqual(result.
+                         get_counts(circ)['{0:b}'.format(0).zfill(rb_opts['n_qubits'])], shots,
+                         "Error: %d qubit RB does not return the \
+                         ground state back to the ground state" % rb_opts['n_qubits'])
+
+    def test_rb(self):
+        """ Main function of the test """
+
+        # Load simulator
+        backend = qiskit.Aer.get_backend('qasm_simulator')
+
+        # Test up to 6 qubits
+        nq_list = [1, 2, 3, 4, 5, 6]
+
+        for nq in nq_list:
+
+            print("Testing %d qubit RB"%nq)
+
+            for pattern_type in range(3):
+                # See documentation of choose_pattern for the meaning of the different pattern types
+
+                rb_opts = {}
+                rb_opts['nseeds'] = random.randint(1, 3)
+                rb_opts['n_qubits'] = nq
+
+                rb_opts['length_vector'] = self.choose_length_vector()
+                rb_opts['rb_pattern'] = self.choose_pattern(pattern_type, nq)
+                if rb_opts['rb_pattern'] is None:   # if the pattern type is not relevant for nq
+                    continue
+                rb_opts['length_multiplier'] = self.choose_multiplier(len(rb_opts['rb_pattern']))
+
+                # Generate the sequences
+                try:
+                    rb_circs, _, rb_opts = rb.randomized_benchmarking_seq(rb_opts)
+                except OSError:
+                    print('Skipping tests for ' + str(nq) + ' qubits because tables are missing')
+                    continue
+
+                # Perform an ideal execution on the generated sequences
+                basis_gates = 'u1,u2,u3,cx' # use U, CX for now
+                shots = 100
+                result = qiskit.execute(rb_circs, backend=backend,
+                                        basis_gates=basis_gates, shots=shots).result()
+
+                # Verify the generated sequences
+                circ_index = 0
+                for seed in range(rb_opts['nseeds']):
+                    for vec_len in rb_opts['length_vector']:
+                        self.assertEqual(rb_circs[circ_index].name,
+                                         'rb_seed_' + str(seed) + '_length_' + str(vec_len),
+                                         'Error: incorrect circuit name')
+                        self.verify_circuit(rb_circs[circ_index], rb_opts, vec_len, result, shots)
+                        circ_index += 1
+
+                self.assertEqual(circ_index, len(rb_circs), "Error: additional circuits exist")
+
+
+if __name__ == '__main__':
+    unittest.main()
