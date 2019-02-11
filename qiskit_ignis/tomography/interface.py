4e8647dfae6017699a2b2f425bfe5130f215d567
Gadi Aleksandrowicz <gadia@il.ibm.com>
2019-02-11 13:59:43 +0200

    Interface for process tomography and corresponding tests
    Signed-off-by: Gadi Aleksandrowicz <gadia@il.ibm.com>

--- qiskit_ignis/tomography/interface.py
+++ qiskit_ignis/tomography/interface.py
@@ -1,14 +1,23 @@
-def perform_state_tomography(circuit, measured_qubits, shots = 5000):
-    import qiskit
-    from qiskit_aer import Aer
-    from .basis.circuits import state_tomography_circuits
-    from .data import tomography_data
-    from .fitters import fitter_data, cvx_fit
+import qiskit
+from qiskit import Aer
+from .basis.circuits import state_tomography_circuits, process_tomography_circuits
+from .data import tomography_data
+from .fitters import fitter_data, state_cvx_fit, process_cvx_fit
 
+def perform_state_tomography(circuit, measured_qubits, shots = 5000):
     qasm_simulator = Aer.get_backend('qasm_simulator')
     tomography_circuits = state_tomography_circuits(circuit, measured_qubits)
     job = qiskit.execute(tomography_circuits, qasm_simulator, shots=shots)
     tomography_data_results = tomography_data(job.result(), tomography_circuits)
-    data, basis = fitter_data(tomography_data_results)
-    rho = cvx_fit(data, basis, trace=1)
+    data, basis, weights = fitter_data(tomography_data_results)
+    rho = state_cvx_fit(data, basis)
     return rho
+
+def perform_process_tomography(circuit, measured_qubits, shots = 4000):
+    qasm_simulator = Aer.get_backend('qasm_simulator')
+    tomography_circuits = process_tomography_circuits(circuit, measured_qubits)
+    job = qiskit.execute(tomography_circuits, qasm_simulator, shots=shots)
+    tomography_data_results = tomography_data(job.result(), tomography_circuits)
+    data, basis, weights = fitter_data(tomography_data_results)
+    choi = process_cvx_fit(data, basis)
+    return choi
