147c5cf1400c25bf7a1013afb495dd6ced1b7da5
Shelly Garion <shelly@il.ibm.com>
2019-02-14 14:21:54 +0200

    updated test_fitters.py

--- tests/rb/test_fitters.py
+++ tests/rb/test_fitters.py
@@ -27,65 +27,50 @@ class TestFitters(unittest.TestCase):
         # Load simulator
         backend = qiskit.Aer.get_backend('qasm_simulator')
 
-        tests = [{'rb_opts':
-                  {'nseeds': 2,
-                   'n_qubits': 3,
-                   'length_vector': [10, 15, 25, 40],
-                   'rb_pattern': [[0], [1, 2]],
-                   'length_multiplier': [1, 2]
+        tests = [
+            {'rb_opts':
+                 {'nseeds': 3,
+                  'length_vector': [1, 3, 4, 7, 10],
+                  'n_qubits': 2,
+                  'rb_pattern': [[0], [1]],
+                  'length_multiplier': 1
                   },
-                  'expected':
-                  {'xdata': [[10, 15, 25, 40], [20, 30, 50, 80]],
-                   'raw_data': [[0.71, 0.58, 0.44, 0.38], [0.58, 0.62, 0.46, 0.3]],
-                   'mean': [0.645, 0.6, 0.45, 0.34],
-                   'std': [0.065, 0.02, 0.01, 0.04],
-                   'fit_params': [[0.75539728, 0.95548813, 0.20966888],
-                                  [0.75539745, 0.97749087, 0.2096672]],
-                   'fit_err': [[0.08205335, 0.02344961, 0.15338413],
-                               [0.08205379, 0.01199477, 0.15338564]],
-                   'epc': [0.022255934833535218, 0.016881850903803647],
-                   'epc_err': [0.000546205525578543, 0.00020715691181849986]
+             'expected':
+                 {'xdata': [[1, 3, 4, 7, 10], [1, 3, 4, 7, 10]],
+                  'raw_data': [[1.0, 0.98, 0.97, 0.97, 0.94],
+                               [1.0, 0.99, 0.97, 0.95, 0.94],
+                               [0.99, 0.98, 0.98, 0.94, 0.93]],
+                  'mean': [0.99666667, 0.98333333, 0.97333333, 0.95333333, 0.93666667],
+                  'std': [0.00471405, 0.00471405, 0.00471405, 0.01247219, 0.00471405],
+                  'fit_params': [[0.21033493, 0.96121183, 0.79492823],
+                                 [0.21033493, 0.96121183, 0.79492823]],
+                  'fit_err': [[0.1071722 , 0.02469688, 0.1090674 ],
+                              [0.1071722, 0.02469688, 0.1090674]],
+                  'epc': [0.019394082609075713, 0.019394082609075713],
+                  'epc_err': [0.000498301633611352, 0.000498301633611352]
                   }
-                 },
-                 {'rb_opts':
-                  {'nseeds': 1,
-                   'n_qubits': 3,
-                   'length_vector': [10, 15, 28, 45],
-                   'rb_pattern': [[0], [1, 2]],
-                   'length_multiplier': [1, 2]
-                  },
-                  'expected':
-                  {'xdata': [[10, 15, 28, 45], [20, 30, 56, 90]],
-                   'raw_data': [[0.65, 0.59, 0.42, 0.32]],
-                   'mean': [0.65, 0.59, 0.42, 0.32],
-                   'std': None,
-                   'fit_params': [[0.67621574, 0.96316045, 0.19225674],
-                                  [0.6762157, 0.98140737, 0.19225687]],
-                   'fit_err': [[0.05350837, 0.01428833, 0.1000942],
-                               [0.05350833, 0.00727951, 0.10009414]],
-                   'epc': [0.018419777304980955, 0.013944472137794234],
-                   'epc_err': [0.00027325441942194396, 0.00010343200410712699]
-                  }
-                 },
-                 {'rb_opts':
-                  {'nseeds': 2,
-                   'n_qubits': 3,
-                   'length_vector': [10, 15, 25, 40],
-                   'rb_pattern': [[0, 1, 2]],
-                   'length_multiplier': [1]
-                  },
-                  'expected':
-                  {'xdata': [[10, 15, 25, 40]],
-                   'raw_data': [[0.6, 0.49, 0.31, 0.21], [0.57, 0.55, 0.41, 0.25]],
-                   'mean': [0.585, 0.52, 0.36, 0.23],
-                   'std': [0.015, 0.03, 0.05, 0.02],
-                   'fit_params': [[0.92395288, 0.97838208, -0.15621131]],
-                   'fit_err': [[0.31235316, 0.01469988, 0.364496]],
-                   'epc': [0.018915683021742555],
-                   'epc_err': [0.0002842022079552383]
-                  }
-                 }
-                ]
+             },
+            {'rb_opts':
+                {'nseeds': 3,
+                'length_vector': [1, 10, 20, 30, 40, 50],
+                'n_qubits': 1,
+                'rb_pattern': [[0]],
+                'length_multiplier': 1
+                },
+            'expected':
+                {'xdata': [[1, 10, 20, 30, 40, 50]],
+                'raw_data': [[0.98, 0.97, 0.93, 0.85, 0.84, 0.85],
+                             [0.97, 0.95, 0.96, 0.89, 0.84, 0.8],
+                             [0.97, 0.95, 0.93, 0.87, 0.89, 0.88]],
+                'mean': [0.97333333, 0.95666667, 0.94, 0.87, 0.85666667, 0.84333333],
+                'std': [0.00471405, 0.00942809, 0.01414214, 0.01632993, 0.02357023, 0.03299832],
+                'fit_params': [[ 2.,  0.99853465, -1.02171782]],
+                'fit_err': [[3.10537620e+01, 2.33472957e-02, 3.10566719e+01]],
+                'epc': [0.0007326746000217987],
+                'epc_err': [1.7131073541769797e-05]
+                }
+             }
+        ]
 
         for tst_index, tst in enumerate(tests):
 
@@ -94,11 +79,7 @@ class TestFitters(unittest.TestCase):
 
             # Generate the sequences
             rb_opts = tst['rb_opts'].copy()
-            try:
-                rb_circs, xdata = rb.randomized_benchmarking_seq(**rb_opts)
-            except OSError:
-                print('Skipping test no. ' + str(tst_index) + ' because tables are missing')
-                continue
+            rb_circs, xdata = rb.randomized_benchmarking_seq(**rb_opts)
 
             # Define a noise model
             noise_model = NoiseModel()
@@ -107,7 +88,7 @@ class TestFitters(unittest.TestCase):
             noise_model.add_all_qubit_quantum_error(depolarizing_error(0.002, 2), 'cx')
 
             # Perform an execution on the generated sequences
-            basis_gates = ['u1','u2','u3','cx'] # use U, CX for now
+            basis_gates = 'u1,u2,u3,cx' # use U, CX for now #Shelly: changed format to fit qiskit current version
             shots = 100
             result_list = []
             for i in range(rb_opts['nseeds']):
@@ -137,7 +118,6 @@ class TestFitters(unittest.TestCase):
                 self.assertTrue(all(np.isclose(a, b) for a, b in
                                     zip(ydata['std'], tst['expected']['std'])),
                                 'Incorrect std in test no. ' + str(tst_index))
-
             fit = fitters.calc_rb_fit(xdata, ydata, rb_opts['rb_pattern'])
 
             self.assertTrue(all(np.isclose(a, b) for c, d in
