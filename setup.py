7247ebbb4d7943c20856a72ccdaacd1517ed8807
Matthew Treinish <mtreinish@kortar.org>
2019-02-19 16:14:16 -0500

    Add some boilerplate to the repo
    This commit adds some boilerplate files to the repo that were missing.
    Things like the license file, setup.py tox.ini, etc.

---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2017, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+from setuptools import setup, find_packages
+
+
+requirements = [
+    "numpy>=1.13,<1.16",
+    "qiskit-terra>=0.7.0",
+    "scipy>=0.19,!=0.19.1",
+]
+
+
+setup(
+    name="qiskit-ignis",
+    version="0.8.0",
+    description="Qiskit tools for quantum information science",
+    url="https://github.com/Qiskit/qiskit-ignis",
+    author="Qiskit Development Team",
+    author_email="qiskit@qiskit.org",
+    license="Apache 2.0",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords="qiskit sdk quantum",
+    packages=find_packages(exclude=['test*']),
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires=">=3.5"
+)
