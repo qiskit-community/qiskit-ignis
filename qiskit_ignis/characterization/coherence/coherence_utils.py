d31440ebf1064e6b56f007772dedd9a626209d08
David McKay <dcmckay@us.ibm.com>
2019-02-19 01:35:01 -0500

    Add function to convert between time and gates

--- qiskit_ignis/characterization/coherence/coherence_utils.py
+++ qiskit_ignis/characterization/coherence/coherence_utils.py
@@ -5,6 +5,8 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
+import numpy as np
+
 """
 Utilities for circuits generation
 """
@@ -28,3 +30,18 @@ def pad_id_gates(circuit, qr, num_of_id_gates):
         circuit.iden(qr)
 
     return circuit
+
+def time_to_ngates(times, gate_time):
+    """
+    A function to convert a list of times into an integer list of gates.
+
+    Args:
+       times: A list of times (in the same unit of time as used to
+       specify gate_time)
+       gate_time: the gate_time
+
+    Returns:
+       num_gates: integer list of the number of gates
+    """
+
+    return (np.round(np.array(times)/gate_time)).astype(int)
