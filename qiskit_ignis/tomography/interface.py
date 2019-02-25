66b8d74fe955036e9bd88cf21ec4e4b9a8618e52
Gadi Aleksandrowicz <gadia@il.ibm.com>
2019-02-25 18:08:36 +0200

    Process tomography interface enhancement
    Signed-off-by: Gadi Aleksandrowicz <gadia@il.ibm.com>

--- qiskit_ignis/tomography/interface.py
+++ qiskit_ignis/tomography/interface.py
@@ -1,6 +1,7 @@
 import qiskit
 from qiskit import Aer
 from qiskit.quantum_info import state_fidelity
+from qiskit.tools.qi.qi import outer
 from .basis.circuits import state_tomography_circuits, process_tomography_circuits
 from .data import tomography_data
 from .fitters import fitter_data, state_cvx_fit, process_cvx_fit, state_mle_fit, process_mle_fit
@@ -42,11 +43,39 @@ def perform_state_tomography(circuit, measured_qubits,
 
     return result
 
-def perform_process_tomography(circuit, measured_qubits, shots = 4000):
-    qasm_simulator = Aer.get_backend('qasm_simulator')
+def perform_process_tomography(circuit, measured_qubits,
+                             backend = None,
+                             fitter = 'cvx',
+                             ideal = True,
+                             fidelity = True,
+                             shots = 4000,):
+    if backend is None:
+        backend = Aer.get_backend('qasm_simulator')
     tomography_circuits = process_tomography_circuits(circuit, measured_qubits)
-    job = qiskit.execute(tomography_circuits, qasm_simulator, shots=shots)
+    job = qiskit.execute(tomography_circuits, backend, shots=shots)
     tomography_data_results = tomography_data(job.result(), tomography_circuits)
     data, basis, weights = fitter_data(tomography_data_results)
-    choi = process_cvx_fit(data, basis)
-    return choi
+    choi = None
+    if fitter == 'cvx':
+        try:
+            choi = process_cvx_fit(data, basis)
+        except Exception as e:
+            print("CVX run failed: {}, attempting MLE fit".format(e))
+    if fitter == 'mle' or choi is None:
+        choi = process_mle_fit(data, basis)
+
+    if ideal == False and fidelity == False:
+        return choi
+
+    result = {'choi': choi}
+    if ideal == True:
+        job = qiskit.execute(circuit, Aer.get_backend('unitary_simulator'))
+        ideal_unitary = job.result().get_unitary(circuit)
+        ideal_choi = outer(ideal_unitary.ravel(order='F'))
+        result['ideal_choi'] = ideal_choi
+        if fidelity == True:
+            n = len(measured_qubits)
+            fidelity_value = state_fidelity(ideal_choi / 2**n, choi / 2**n)
+            result['fidelity'] = fidelity_value
+    return result
+
