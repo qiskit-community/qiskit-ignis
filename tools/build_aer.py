a8a4dc6c267c3dc3a10eea56d20f9cdf7fe27143
Matthew Treinish <mtreinish@kortar.org>
2019-04-24 15:48:09 -0400

    Tweak script and install scikit-build

--- tools/build_aer.py
+++ tools/build_aer.py
@@ -13,11 +13,6 @@ if os.name == 'nt' or sys.platform == 'darwin':
                      'git+https://github.com/Qiskit/qiskit-aer.git'])
 
 if sys.platform == 'linux' or sys.platform == 'linux2':
-    git_dir = tempfile.mkdtemp()
-    try:
-        subprocess.call(['git', 'clone',
-                         'https://github.com/Qiskit/qiskit-aer.git', git_dir])
-        subprocess.call([sys.executable, 'setup.py', 'install', '--',
-                         '-DCMAKE_CXX_COMPILER=g++-7'], cwd=git_dir)
-    finally:
-        shutil.rmtree(git_dir)
+    subprocess.call(['pip', 'install', '-U',
+                     'git+https://github.com/Qiskit/qiskit-aer.git', '--',
+                     '-DCMAKE_CXX_COMPILER=g++-7')
