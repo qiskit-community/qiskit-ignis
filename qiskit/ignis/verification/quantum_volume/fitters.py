ad55f8f30d48cba8f2ff0aa3dd9455a1f33c1264
David McKay <dcmckay@us.ibm.com>
2019-04-30 05:16:13 -0400

    Fixed copyright, fixed lint

--- qiskit/ignis/verification/quantum_volume/fitters.py
+++ qiskit/ignis/verification/quantum_volume/fitters.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2019, IBM.
+# This code is part of Qiskit.
 #
-# This source code is licensed under the Apache License, Version 2.0 found in
-# the LICENSE.txt file in the root directory of this source tree.
+# (C) Copyright IBM 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
 
 """
 Functions used for the analysis of quantum volume results.
@@ -35,7 +42,6 @@ class QVFitter:
             qubit_lists: list of qubit lists (what was passed to the
             circuit generation)
         """
-
 
         self._qubit_lists = qubit_lists
         self._depths = [len(l) for l in qubit_lists]
