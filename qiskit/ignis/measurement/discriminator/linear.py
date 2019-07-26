31bd583ab3e7ae88023c5ca85148154c85fefa96
Daniel Egger <deg@zurich.ibm.com>
2019-07-26 16:44:36 +0200

    * deleted discriminator.py. * Modified the linear discriminator to inherit from BaseFitter.

--- qiskit/ignis/measurement/discriminator/linear.py
+++ qiskit/ignis/measurement/discriminator/linear.py
@@ -15,83 +15,91 @@
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-from qiskit.result import Result
-from qiskit.ignis.discriminator.discriminator import AbstractDiscriminator
-from qiskit.result.models import ExperimentResult
+from qiskit.ignis.characterization.fitters import BaseFitter
+from qiskit.exceptions import QiskitError
 
 
-class LinearIQDiscriminator(AbstractDiscriminator):
-    """
-    A linear discriminant analysis for IQ data based on scikit learn's LinearDiscriminantAnalysis.
-    """
+class LinearIQDiscriminationFitter(BaseFitter):
 
-    def __init__(self, result: Result, cal_circuits: list, data_circuits: list,
-                 cal_circuit_expected, **discriminator_parameters):
+    def __init__(self, cal_results, discriminator_parameters,
+                 qubits, circuit_names, expected_states):
+        """
+        Args:
+            cal_results: calibration results, list of qiskit.Result or qiskit.Result
+            discriminator_parameters: parameters for the discriminator.
+            qubits: the qubits for which we want to discriminate.
+            circuit_names: The names of the circuits in cal_results.
+            expected_states: a list that should have the same length as cal_results.
+                If cal_results is a Result and not a list then exoected_states should
+                be a string or a float or an int.
+        """
 
-        super().__init__(result, cal_circuits, data_circuits,
-                         cal_circuit_expected, **discriminator_parameters)
-
-        self.lda = LinearDiscriminantAnalysis(
+        lda = LinearDiscriminantAnalysis(
             solver=discriminator_parameters.get('solver', 'svd'),
             shrinkage=discriminator_parameters.get('shrinkage', None),
             store_covariance=discriminator_parameters.get('store_covariance', False),
             tol=discriminator_parameters.get('tol', 1.0e-4))
 
-    def _extract_calibration(self):
+        # Sanity checks
+        if isinstance(cal_results, list) and isinstance(expected_states, list):
+            if len(cal_results) != len(expected_states):
+                raise QiskitError('Inconsistent number of results and expected results.')
+
+        description = 'Linear IQ discriminator for measurement level 1.'
+
+        _expected_state = []
+        if isinstance(expected_states, list):
+            for es in expected_states:
+                _expected_state.append(es)
+        else:
+            _expected_state.append(expected_states)
+
+        BaseFitter.__init__(self, description, cal_results, None, qubits, lda, None,
+                            None, circuit_names, expected_state=_expected_state)
+
+    def add_data(self, result, recalc=True, refit=True, expected_state=None):
         """
-        Get the measured features corresponding to the calibration circuits.
+        Overrides method of base class.
+        Args:
+            result: the Result obtained from e.g. backend.run().result()
+            recalc: this parameter is irrelevant and only needed for Liskov principle
+            refit: refit the discriminator if True.
+            expected_state: the expected state of the discriminator.
+        """
+        self._expected_state.append(expected_state)
+        self._backend_result_list.append(result)
+
+        if refit:
+            self.fit_data()
+
+    def _calc_data(self):
+        """
+        Extract the measured IQ points (i.e. features) from the list of
+        backend results and combine them into a list of lists.
         Returns:
             x a list of lists. Each sublist corresponds to the IQ points
-            for all the qubits.
+            for all the qubits. Each sublist therefore corresponds to an
+            expected result stored in self._expected_state.
         """
-        x = []
-        for circuit in self._cal_circuits:
-            x.append(self._get_features(circuit))
+        self._xdata = []
+        for circuit in self._circuit_names:
+            features = []
+            for result in self._backend_result_list:
+                try:
+                    iq_data = result.get_memory(circuit)
+                    features.extend([np.real(iq_data), np.imag(iq_data)])
+                except (QiskitError, KeyError):
+                    pass
 
-        return x
+            self._xdata.append(features)
 
-    def _extract_data(self):
+    def fit_data(self, qid=-1, p0=None, bounds=None, series=None):
         """
-        Get the measured features corresponding to the measured circuits.
-        Returns:
-            x a list of lists. Each sublist corresponds to the IQ points
-            for all the qubits.
+        Fit the Linear Discriminant.
+        Args:
+            qid: not needed
+            p0: not needed
+            bounds: not needed
+            series: not needed
         """
-        x = []
-        for circuit in self._data_circuits:
-            x.append(self._get_features(circuit))
-
-        return x
-
-    def _get_features(self, circuit):
-        """Helper routine to extract the IQ points for all qubits."""
-        features = []
-        for result in self.result.get_memory(circuit):
-            features.extend([np.real(result), np.imag(result)])
-
-        return features
-
-    def fit(self):
-        """Trains the discriminator to the calibration data."""
-        self.lda.fit(self._extract_calibration(), self._cal_circuits_expected)
-        self.fitted = True
-
-    def discriminate(self):
-
-        if not self.fitted:
-            self.fit()
-
-        x = self._extract_data()
-        y = self.lda.predict(x)
-
-        # TODO Should this be
-        shots = None  # TODO Get shots
-        self.result.append(ExperimentResult(shots, True, y, meas_level=2))
-        # TODO or this
-        counter = 0
-        for circuit in self._data_circuits:
-            shots = None  # TODO extracted somehow from self.result
-            self.result.append(ExperimentResult(shots, True, y[counter], meas_level=2))
-            counter += 1
-
-        return self.result
+        self._fit_fun.fit(self._xdata, self._expected_state)
