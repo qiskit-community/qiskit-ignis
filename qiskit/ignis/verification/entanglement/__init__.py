4a68f0e07e17e694af56baa5533c3411c11a4b51
Yehuda Naveh <naveh@il.ibm.com>
2019-09-09 19:48:01 +0300

    Update __init__.py

--- qiskit/ignis/verification/entanglement/__init__.py
+++ qiskit/ignis/verification/entanglement/__init__.py
@@ -1,8 +1,8 @@
 '''
 In this package, there are three modules, linear.py,
-parallellize.py, and analysis.py.It may be more suitable
-to put these in Terra rather than Ignis. The most
-important module, parallellize.py provides methods to
+parallellize.py, and analysis.py. Once Terra completes design
+for circuit repository, may be more suitable to move it there. 
+The most important module, parallellize.py provides methods to
 parallellize CNOT gates in the preparation of the GHZ State,
 which results to the GHZ State having a much higher fidelity
 then a normal "linear" CNOT gate preparation of the
