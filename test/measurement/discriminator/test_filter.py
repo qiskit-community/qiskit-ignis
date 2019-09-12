4917952485adee005d104e1c096b6dbe7c48ec60
Daniel Egger <deg@zurich.ibm.com>
2019-09-12 10:35:03 +0200

    * Move the IQ discriminators to their own python file.

--- test/measurement/discriminator/test_filter.py
+++ test/measurement/discriminator/test_filter.py
@@ -19,7 +19,7 @@ import qiskit
 from qiskit import Aer
 from qiskit.ignis.measurement.discriminator.filters import DiscriminationFilter
 from qiskit.result.models import ExperimentResultData
-from qiskit.ignis.measurement.discriminator.discriminators import \
+from qiskit.ignis.measurement.discriminator.iq_discriminators import \
     LinearIQDiscriminator
 import test.utils as utils
 from qiskit.ignis.mitigation.measurement import circuits
