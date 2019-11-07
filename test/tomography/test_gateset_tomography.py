5dc4c5ea1040f8f0fe21d49e5e1706eb58896192
Gadi Aleksandrowicz <gadia@il.ibm.com>
2019-11-07 11:40:53 +0200

    Added (failing) test for GST with Pauli X noise

--- test/tomography/test_gateset_tomography.py
+++ test/tomography/test_gateset_tomography.py
@@ -22,15 +22,20 @@ from qiskit.compiler import transpile, assemble
 from qiskit.ignis.verification.tomography.fitters.gateset_fitter import gateset_linear_inversion
 from qiskit.ignis.verification.tomography.basis.circuits import gateset_tomography_circuits
 from qiskit.ignis.verification.tomography.fitters.base_fitter import TomographyFitter
-from qiskit.quantum_info import state_fidelity
+from qiskit.providers.aer.noise.errors.quantum_error import QuantumError
+from qiskit.providers.aer.noise import NoiseModel
+from qiskit.quantum_info import Kraus, PTM
 
 class TestGatesetTomography(unittest.TestCase):
-    def collect_tomography_data(self, shots=10000):
+    def collect_tomography_data(self, shots=10000, noise_model=None):
         backend_qasm = Aer.get_backend('qasm_simulator')
         circuits = gateset_tomography_circuits()
-        transpiled_circuits = transpile(circuits, backend=backend_qasm)
+        if noise_model:
+            transpiled_circuits = transpile(circuits, backend=backend_qasm, basis_gates=noise_model.basis_gates)
+        else:
+            transpiled_circuits = transpile(circuits, backend=backend_qasm)
         qobj = assemble(transpiled_circuits, shots=shots)
-        result = backend_qasm.run(qobj).result()
+        result = backend_qasm.run(qobj, noise_model=noise_model).result()
         t = TomographyFitter(result, circuits)
         return t._data
 
@@ -53,18 +58,66 @@ class TestGatesetTomography(unittest.TestCase):
         # Z-basis measurement in PTM
         E = np.array([[1, 0, 0, 1]])
 
-        data = self.collect_tomography_data(shots=10000)
-        gates, gate_labels = zip(*gateset_linear_inversion(data))
         # linear inversion should result in gates of the form B^-1*G*B where the columns
         # of B are F_k * rho, F_k being the SPAM circuits
 
         Fs = [F0, F1, F2, F3]
         B = np.array([(F @ rho).T[0] for F in Fs]).T
+
+        # perform linear inversion
+        data = self.collect_tomography_data(shots=10000)
+        print(data)
+        gates, gate_labels = zip(*gateset_linear_inversion(data))
+
         expected_gates = [np.linalg.inv(B) @ G @ B for G in [G0, G1, G2]]
         for expected_gate, gate, label in zip(expected_gates, gates, gate_labels):
             hs_distance = sum([np.abs(x)**2 for x in np.nditer(expected_gate - gate)])
             msg = "Failure on gate {}: Expected gate = \n{}\n vs Actual gate = \n{}".format(label, expected_gate, gate)
             self.assertAlmostEqual(hs_distance, 0, delta = 0.1, msg=msg)
+
+        # now add noise
+        # gamma = 0.5
+        # A0 = [[1, 0], [0, np.sqrt(1 - gamma)]]
+        # A1 = [[0, np.sqrt(gamma)], [0, 0]]
+        # noise = QuantumError([([{'name': 'kraus', 'qubits': [0], 'params': [A0, A1]}], 1)])
+
+        A0 = [[0, 1], [1, 0]]
+        noise = QuantumError([([{'name': 'kraus', 'qubits': [0], 'params': [A0]}], 1)])
+
+        noise_model = NoiseModel()
+        noise_model.add_all_qubit_quantum_error(noise, ['u1', 'u2', 'u3'])
+        data = self.collect_tomography_data(shots=10000, noise_model=noise_model)
+        print(data)
+        gates, gate_labels = zip(*gateset_linear_inversion(data))
+
+        noise_PTM = PTM(Kraus([A0]))._data
+        print(noise_PTM)
+        noisy_gates = [noise_PTM @ G for G in [G0, G1, G2]]
+        print(noisy_gates)
+        expected_gates = [np.linalg.inv(B) @ G @ B for G in noisy_gates]
+        for expected_gate, gate, label in zip(expected_gates, gates, gate_labels):
+            hs_distance = sum([np.abs(x) ** 2 for x in np.nditer(expected_gate - gate)])
+            msg = "Failure on gate {}: Expected gate = \n{}\n vs Actual gate = \n{}".format(label, expected_gate, gate)
+            self.assertAlmostEqual(hs_distance, 0, delta=0.1, msg=msg)
+
+#         from qiskit import QuantumRegister
+#         from qiskit import ClassicalRegister
+#         from qiskit import QuantumCircuit
+#
+#         backend_qasm = Aer.get_backend('qasm_simulator')
+#         qubit = QuantumRegister(1)
+#         clbit = ClassicalRegister(1)
+#         circuit = QuantumCircuit(qubit, clbit)
+#         circuit.x(qubit)
+#         circuit.u3(0, 0, 0, qubit)
+#         circuit.measure(qubit, clbit)
+#         print(circuit)
+#         transpiled_circuits = transpile([circuit], backend=backend_qasm, basis_gates=noise_model.basis_gates)
+#         qobj = assemble(transpiled_circuits, shots=1000)
+#         result = backend_qasm.run(qobj, noise_model=noise_model).result()
+# #        result = backend_qasm.run(qobj).result()
+#         print(result.results[0].data.counts)
+
 
 
 if __name__ == '__main__':
