606a01c5c34a5ffc5119813f7f9de0b4ba991b1d
Gadi Aleksandrowicz <gadia@il.ibm.com>
2019-11-11 13:33:35 +0200

    GST linear inversion tests with noise are now working

--- test/tomography/test_gateset_tomography.py
+++ test/tomography/test_gateset_tomography.py
@@ -30,28 +30,12 @@ class TestGatesetTomography(unittest.TestCase):
     def collect_tomography_data(self, shots=10000, noise_model=None):
         backend_qasm = Aer.get_backend('qasm_simulator')
         circuits = gateset_tomography_circuits()
-        if noise_model:
-            transpiled_circuits = transpile(circuits, backend=backend_qasm, basis_gates=noise_model.basis_gates)
-        else:
-            transpiled_circuits = transpile(circuits, backend=backend_qasm)
-        qobj = assemble(transpiled_circuits, shots=shots)
+        qobj = assemble(circuits, shots=shots)
         result = backend_qasm.run(qobj, noise_model=noise_model).result()
         t = TomographyFitter(result, circuits)
         return t._data
 
-
-    def test_linear_inversion(self):
-        # based on linear inversion in	arXiv:1310.4492
-        # PTM representation of Id, X rotation by 90 degrees, Y rotation by 90 degrees
-        G0 = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        G1 = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, -1], [0, 0, 1, 0]])
-        G2 = np.array([[1, 0, 0, 0], [0, 0, 0, -1], [0, 0, 1, 0], [0, 1, 0, 0]])
-
-        F0 = G0
-        F1 = G1
-        F2 = G2
-        F3 = G1 @ G1
-
+    def linear_inversion_on_gates(self, Gs, Fs, shots=10000, noise_model=None):
         # in PTM: [(1,0),(0,0)] == 0.5 I + 0.5 Z
         rho = np.array([[0.5], [0], [0], [0.5]])
 
@@ -61,64 +45,49 @@ class TestGatesetTomography(unittest.TestCase):
         # linear inversion should result in gates of the form B^-1*G*B where the columns
         # of B are F_k * rho, F_k being the SPAM circuits
 
-        Fs = [F0, F1, F2, F3]
         B = np.array([(F @ rho).T[0] for F in Fs]).T
-
         # perform linear inversion
-        data = self.collect_tomography_data(shots=10000)
-        print(data)
+        data = self.collect_tomography_data(shots=shots,noise_model=noise_model)
         gates, gate_labels = zip(*gateset_linear_inversion(data))
 
-        expected_gates = [np.linalg.inv(B) @ G @ B for G in [G0, G1, G2]]
-        for expected_gate, gate, label in zip(expected_gates, gates, gate_labels):
-            hs_distance = sum([np.abs(x)**2 for x in np.nditer(expected_gate - gate)])
-            msg = "Failure on gate {}: Expected gate = \n{}\n vs Actual gate = \n{}".format(label, expected_gate, gate)
-            self.assertAlmostEqual(hs_distance, 0, delta = 0.1, msg=msg)
-
-        # now add noise
-        # gamma = 0.5
-        # A0 = [[1, 0], [0, np.sqrt(1 - gamma)]]
-        # A1 = [[0, np.sqrt(gamma)], [0, 0]]
-        # noise = QuantumError([([{'name': 'kraus', 'qubits': [0], 'params': [A0, A1]}], 1)])
-
-        A0 = [[0, 1], [1, 0]]
-        noise = QuantumError([([{'name': 'kraus', 'qubits': [0], 'params': [A0]}], 1)])
-
-        noise_model = NoiseModel()
-        noise_model.add_all_qubit_quantum_error(noise, ['u1', 'u2', 'u3'])
-        data = self.collect_tomography_data(shots=10000, noise_model=noise_model)
-        print(data)
-        gates, gate_labels = zip(*gateset_linear_inversion(data))
-
-        noise_PTM = PTM(Kraus([A0]))._data
-        print(noise_PTM)
-        noisy_gates = [noise_PTM @ G for G in [G0, G1, G2]]
-        print(noisy_gates)
-        expected_gates = [np.linalg.inv(B) @ G @ B for G in noisy_gates]
+        expected_gates = [np.linalg.inv(B) @ G @ B for G in Gs]
         for expected_gate, gate, label in zip(expected_gates, gates, gate_labels):
             hs_distance = sum([np.abs(x) ** 2 for x in np.nditer(expected_gate - gate)])
             msg = "Failure on gate {}: Expected gate = \n{}\n vs Actual gate = \n{}".format(label, expected_gate, gate)
             self.assertAlmostEqual(hs_distance, 0, delta=0.1, msg=msg)
 
-#         from qiskit import QuantumRegister
-#         from qiskit import ClassicalRegister
-#         from qiskit import QuantumCircuit
-#
-#         backend_qasm = Aer.get_backend('qasm_simulator')
-#         qubit = QuantumRegister(1)
-#         clbit = ClassicalRegister(1)
-#         circuit = QuantumCircuit(qubit, clbit)
-#         circuit.x(qubit)
-#         circuit.u3(0, 0, 0, qubit)
-#         circuit.measure(qubit, clbit)
-#         print(circuit)
-#         transpiled_circuits = transpile([circuit], backend=backend_qasm, basis_gates=noise_model.basis_gates)
-#         qobj = assemble(transpiled_circuits, shots=1000)
-#         result = backend_qasm.run(qobj, noise_model=noise_model).result()
-# #        result = backend_qasm.run(qobj).result()
-#         print(result.results[0].data.counts)
+    def test_linear_inversion(self):
+        # based on linear inversion in	arXiv:1310.4492
+        # PTM representation of Id, X rotation by 90 degrees, Y rotation by 90 degrees
+        G0 = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        G1 = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, -1], [0, 0, 1, 0]])
+        G2 = np.array([[1, 0, 0, 0], [0, 0, 0, -1], [0, 0, 1, 0], [0, 1, 0, 0]])
 
+        Gs = [G0, G1, G2]
+        Fs = [Gs[0], Gs[1], Gs[2], Gs[1] @ Gs[1]]
+        self.linear_inversion_on_gates(Gs, Fs)
 
+        # Pauli X noise
+        A0 = np.array([[0, 1], [1, 0]])
+        noise_PTM = PTM(Kraus([A0]))._data
+        Gs = [G0, noise_PTM @ G1, noise_PTM @ G2]
+        Fs = [Gs[0], Gs[1], Gs[2], Gs[1] @ Gs[1]]
+        noise = QuantumError([([{'name': 'kraus', 'qubits': [0], 'params': [A0]}], 1)])
+        noise_model = NoiseModel()
+        noise_model.add_all_qubit_quantum_error(noise, ['u1', 'u2', 'u3'])
+        self.linear_inversion_on_gates(Gs, Fs, noise_model=noise_model)
+
+        # Amplitude Damping noise
+        gamma = 0.05
+        A0 = [[1, 0], [0, np.sqrt(1 - gamma)]]
+        A1 = [[0, np.sqrt(gamma)], [0, 0]]
+        noise_PTM = PTM(Kraus([A0, A1]))._data
+        Gs = [G0, noise_PTM @ G1, noise_PTM @ G2]
+        Fs = [Gs[0], Gs[1], Gs[2], Gs[1] @ Gs[1]]
+        noise = QuantumError([([{'name': 'kraus', 'qubits': [0], 'params': [A0, A1]}], 1)])
+        noise_model = NoiseModel()
+        noise_model.add_all_qubit_quantum_error(noise, ['u1', 'u2', 'u3'])
+        self.linear_inversion_on_gates(Gs, Fs, noise_model=noise_model)
 
 if __name__ == '__main__':
     unittest.main()
