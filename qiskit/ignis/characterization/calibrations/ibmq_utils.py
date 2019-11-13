be3b7b960ef928a95404acf7c2805e4aa6a446dc
knzwnao <knzwnao@jp.ibm.com>
2019-11-13 15:29:10 +0900

    fix FC rotation direction

--- qiskit/ignis/characterization/calibrations/ibmq_utils.py
+++ qiskit/ignis/characterization/calibrations/ibmq_utils.py
@@ -140,9 +140,9 @@ def update_u_gates(drag_params, pi2_pulse_schedules=None,
         else:
             _u2_group = (drive_ch, )
 
-        u2_fc1s = [parametrized_fc('P1', np.pi/2, ch, 0)
+        u2_fc1s = [parametrized_fc('P1', -np.pi/2, ch, 0)
                    for ch in _u2_group]
-        u2_fc2s = [parametrized_fc('P0', -np.pi/2, ch, pulse_dur)
+        u2_fc2s = [parametrized_fc('P0', np.pi/2, ch, pulse_dur)
                    for ch in _u2_group]
 
         # find channel dependency for u2
