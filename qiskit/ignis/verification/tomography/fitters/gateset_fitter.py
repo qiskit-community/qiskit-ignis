662bf53015582f214f7fc1af8d8aebb9afff306c
Gadi Aleksandrowicz <gadia@il.ibm.com>
2019-11-25 14:43:45 +0200

    Initial version of MLE objective function

--- qiskit/ignis/verification/tomography/fitters/gateset_fitter.py
+++ qiskit/ignis/verification/tomography/fitters/gateset_fitter.py
@@ -15,6 +15,7 @@
 # pylint: disable=missing-docstring
 
 import numpy as np
+import itertools
 from qiskit.ignis.verification.tomography.basis.gatesetbasis import StandardGatesetBasis
 
 def compute_probs(data):
@@ -31,7 +32,7 @@ def gateset_linear_inversion(data, gateset_basis='Standard GST'):
     probs = compute_probs(data)
     n = len(gateset_basis.spam_labels)
     m = len(gateset_basis.gate_labels)
-    id_matrix = np.zeros((n,n))
+    gram_matrix = np.zeros((n,n))
     gate_matrices = []
     for i in range(m):
         gate_matrices.append(np.zeros((n,n)))
@@ -40,12 +41,63 @@ def gateset_linear_inversion(data, gateset_basis='Standard GST'):
         for j in range(n): # column
             F_i = gateset_basis.spam_labels[i]
             F_j = gateset_basis.spam_labels[j]
-            id_matrix[i][j] = probs[(F_i, F_j)]
+            gram_matrix[i][j] = probs[(F_i, F_j)]
 
             for k in range(m): #gate
                 G_k = gateset_basis.gate_labels[k]
                 gate_matrices[k][i][j] = probs[(F_i, G_k, F_j)]
-    id_inverse = np.linalg.inv(id_matrix)
+    gram_inverse = np.linalg.inv(gram_matrix)
 
-    gates = [id_inverse @ gate_matrix for gate_matrix in gate_matrices]
+    gates = [gram_inverse @ gate_matrix for gate_matrix in gate_matrices]
     return list(zip(gates,gateset_basis.gate_labels))
+
+
+class GST_Optimize():
+    def __init__(self, Gs, Fs, probs, qubits=1):
+        self.probs = probs
+        self.Gs = Gs
+        self.Fs = Fs
+        self.Fs_names = list(Fs.keys())
+        self.qubits = qubits
+        self.obj_fn_data = self.compute_objective_function_data()
+
+    def compute_objective_function_data(self):
+        """The objective function is sum_{ijk}(<|E*R_Fi*G_k*R_Fj*Rho|>-m_{ijk})^2
+           We expand R_Fi*G_k*R_Fj to a sequence of G-gates and store the indices
+           we also obtain the m_{ijk} value from the probs list
+           all that remains when computing the function is thus performing
+           the matrix multiplications and remaining algebra
+        """
+        m = len(self.Fs)
+        n = len(self.Gs)
+        obj_fn_data = []
+        for (i, j) in itertools.product(range(m), repeat=2):
+            for k in range(n):
+                m_ijk = (self.probs[(self.Fs_names[i], self.Gs[k], self.Fs_names[j])])
+                matrices = [self.Gs.index(gate) for gate in self.Fs[self.Fs_names[i]]] \
+                           + [k] \
+                           + [self.Gs.index(gate) for gate in self.Fs[self.Fs_names[j]]]
+                obj_fn_data.append((matrices, m_ijk))
+        return obj_fn_data
+
+    def obj_fn(self, x):
+        n = len(self.Gs)
+        d = (2 ** self.qubits) * 2  # for 1 qubit this will be 4, the dimension of density operators
+        expected_var_num = 2 * d + n * d ** 2  # E is 1xd; rho is dx1; each G is dxd
+        if len(x) != expected_var_num:
+            raise (RuntimeError("Expected length of x is {}; got {}".format(expected_var_num, len(x))))
+        E = np.array([x[0:d]])
+        rho = np.array([x[d:2 * d]]).T
+        G_matrices = [np.array([x[2 * d + k * d ** 2 + d * i:2 * d + k * d ** 2 + d * (i + 1)]
+                                for i in range(d)]) for k in range(n)]
+        val = 0
+        for term in self.obj_fn_data:
+            term_val = E
+            for G_index in term[0]:
+                term_val = term_val @ G_matrices[G_index]
+            term_val = term_val @ rho
+            term_val = term_val - term[1]  # m_{ijk}
+            term_val = term_val ** 2
+            val = val + term_val
+
+        return val
--- test/tomography/test_gateset_tomography.py
+++ test/tomography/test_gateset_tomography.py
@@ -20,6 +20,7 @@ import numpy as np
 from qiskit import Aer
 from qiskit.compiler import transpile, assemble
 from qiskit.ignis.verification.tomography.fitters.gateset_fitter import gateset_linear_inversion
+from qiskit.ignis.verification.tomography.fitters.gateset_fitter import GST_Optimize
 from qiskit.ignis.verification.tomography.basis.circuits import gateset_tomography_circuits
 from qiskit.ignis.verification.tomography.fitters.base_fitter import TomographyFitter
 from qiskit.providers.aer.noise.errors.quantum_error import QuantumError
@@ -49,7 +50,6 @@ class TestGatesetTomography(unittest.TestCase):
         # perform linear inversion
         data = self.collect_tomography_data(shots=shots,noise_model=noise_model)
         gates, gate_labels = zip(*gateset_linear_inversion(data))
-
         expected_gates = [np.linalg.inv(B) @ G @ B for G in Gs]
         for expected_gate, gate, label in zip(expected_gates, gates, gate_labels):
             hs_distance = sum([np.abs(x) ** 2 for x in np.nditer(expected_gate - gate)])
@@ -88,6 +88,51 @@ class TestGatesetTomography(unittest.TestCase):
         noise_model = NoiseModel()
         noise_model.add_all_qubit_quantum_error(noise, ['u1', 'u2', 'u3'])
         self.linear_inversion_on_gates(Gs, Fs, noise_model=noise_model)
+
+    def test_mle_function(self):
+        probs = {('F0', 'Id', 'F0'): 1.0, ('F0', 'Id', 'F1'): 0.5036, ('F0', 'Id', 'F2'): 0.5111,
+                 ('F0', 'Id', 'F3'): 0.0, ('F1', 'Id', 'F0'): 0.4921, ('F1', 'Id', 'F1'): 0.0,
+                 ('F1', 'Id', 'F2'): 0.5032, ('F1', 'Id', 'F3'): 0.5049, ('F2', 'Id', 'F0'): 0.4931,
+                 ('F2', 'Id', 'F1'): 0.5078, ('F2', 'Id', 'F2'): 0.0, ('F2', 'Id', 'F3'): 0.4948,
+                 ('F3', 'Id', 'F0'): 0.0, ('F3', 'Id', 'F1'): 0.5024, ('F3', 'Id', 'F2'): 0.5041,
+                 ('F3', 'Id', 'F3'): 1.0, ('F0', 'X_Rot_90', 'F0'): 0.4957, ('F0', 'X_Rot_90', 'F1'): 0.0,
+                 ('F0', 'X_Rot_90', 'F2'): 0.5001, ('F0', 'X_Rot_90', 'F3'): 0.4996, ('F1', 'X_Rot_90', 'F0'): 0.0,
+                 ('F1', 'X_Rot_90', 'F1'): 0.5103, ('F1', 'X_Rot_90', 'F2'): 0.4995, ('F1', 'X_Rot_90', 'F3'): 1.0,
+                 ('F2', 'X_Rot_90', 'F0'): 0.5087, ('F2', 'X_Rot_90', 'F1'): 0.5106, ('F2', 'X_Rot_90', 'F2'): 0.0,
+                 ('F2', 'X_Rot_90', 'F3'): 0.4964, ('F3', 'X_Rot_90', 'F0'): 0.5033, ('F3', 'X_Rot_90', 'F1'): 1.0,
+                 ('F3', 'X_Rot_90', 'F2'): 0.487, ('F3', 'X_Rot_90', 'F3'): 0.4976, ('F0', 'Y_Rot_90', 'F0'): 0.4941,
+                 ('F0', 'Y_Rot_90', 'F1'): 0.4993, ('F0', 'Y_Rot_90', 'F2'): 0.0, ('F0', 'Y_Rot_90', 'F3'): 0.4978,
+                 ('F1', 'Y_Rot_90', 'F0'): 0.5047, ('F1', 'Y_Rot_90', 'F1'): 0.0, ('F1', 'Y_Rot_90', 'F2'): 0.5017,
+                 ('F1', 'Y_Rot_90', 'F3'): 0.4931, ('F2', 'Y_Rot_90', 'F0'): 0.0, ('F2', 'Y_Rot_90', 'F1'): 0.4932,
+                 ('F2', 'Y_Rot_90', 'F2'): 0.5024, ('F2', 'Y_Rot_90', 'F3'): 1.0, ('F3', 'Y_Rot_90', 'F0'): 0.5019,
+                 ('F3', 'Y_Rot_90', 'F1'): 0.5024, ('F3', 'Y_Rot_90', 'F2'): 1.0, ('F3', 'Y_Rot_90', 'F3'): 0.4983,
+                 ('F0', 'F0'): 1.0, ('F0', 'F1'): 0.5068, ('F0', 'F2'): 0.5055, ('F0', 'F3'): 0.0, ('F1', 'F0'): 0.507,
+                 ('F1', 'F1'): 0.0, ('F1', 'F2'): 0.4997, ('F1', 'F3'): 0.4942, ('F2', 'F0'): 0.506,
+                 ('F2', 'F1'): 0.4898, ('F2', 'F2'): 0.0, ('F2', 'F3'): 0.4995, ('F3', 'F0'): 0.0, ('F3', 'F1'): 0.491,
+                 ('F3', 'F2'): 0.5053, ('F3', 'F3'): 1.0, ('F0',): 1.0, ('F1',): 0.5064, ('F2',): 0.5001, ('F3',): 0.0}
+        Gs = ['Id', 'X_Rot_90', 'Y_Rot_90']
+        Fs = {'F0': ('Id',), 'F1': ('X_Rot_90',), 'F2': ('Y_Rot_90',), 'F3': ('X_Rot_90', 'X_Rot_90')}
+        gst = GST_Optimize(Gs, Fs, probs)
+        E = [1, 0, 0, 0]
+        Rho = [0.5, 0, 0, 0.5]
+        G_vals = (np.array([[ 1.00340810e+00, -1.03519151e-04, -2.07629277e-02,
+                 6.47958682e-03],
+               [ 6.30422449e-03,  1.00372520e+00,  3.21007300e-02,
+                -2.27979857e-04],
+               [-1.31900393e-02, -4.94562545e-03,  1.01345762e+00,
+                -1.27923692e-02],
+               [ 3.38239836e-03,  5.08983941e-03, -1.12519332e-02,
+                 1.00644071e+00]]), 'Id'), (np.array([[ 0.00907228, -0.00262494, -0.01875478,  0.99669682],
+               [ 0.98992356,  0.01975411,  0.03653726, -0.988985  ],
+               [-0.01452778, -0.01459403,  0.99913159, -0.02212587],
+               [ 0.00560653,  0.99737474, -0.017777  ,  1.00485468]]), 'X_Rot_90'), (np.array([[-0.00768643,  0.00414623,  0.00337241,  1.00792034],
+               [ 0.01995706,  0.99813113,  0.01564968,  0.00615055],
+               [ 1.00883827, -0.01569707, -0.02251823, -1.02230445],
+               [-0.01231601,  0.01169503,  1.00333928,  1.00056696]]), 'Y_Rot_90')
+
+        x = E + Rho + list(G_vals[0][0].flatten()) + list(G_vals[1][0].flatten()) + list(G_vals[2][0].flatten())
+        result = gst.obj_fn(x)[0][0]
+        self.assertLess(result, 5)
 
 if __name__ == '__main__':
     unittest.main()
