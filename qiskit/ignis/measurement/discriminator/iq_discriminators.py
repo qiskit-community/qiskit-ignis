e89ed465a32cd0244e97238cc448acb7836de2ef
Simon Cross <hodgestar@gmail.com>
2019-12-13 11:28:23 +0200

    Add SklearnIQDiscriminator.

--- qiskit/ignis/measurement/discriminator/iq_discriminators.py
+++ qiskit/ignis/measurement/discriminator/iq_discriminators.py
@@ -496,3 +496,67 @@ class QuadraticIQDiscriminator(IQDiscriminationFitter):
             the discriminated x_data as a list of labels.
         """
         return self._qda.predict(x_data)
+
+
+class SklearnIQDiscriminator(IQDiscriminationFitter):
+    """
+    A generic discriminant analysis discriminator for IQ data that
+    takes an sklearn classifier as an argument.
+    """
+
+    def __init__(self, classifier, cal_results: Union[Result, List[Result]],
+                 qubit_mask: List[int], expected_states: List[str] = None,
+                 standardize: bool = False,
+                 schedules: Union[List[str], List[Schedule]] = None):
+        """
+        Args:
+            classifier:
+                An sklearn classifier to train.
+            cal_results (Union[Result, List[Result]]): calibration results,
+                Result or list of Result used to fit the discriminator.
+            qubit_mask (List[int]): determines which qubit's level 1 data to
+                use in the discrimination process.
+            expected_states (List[str]): a list that should have the same
+                length as schedules. All results in cal_results are used if
+                schedules is None. expected_states must have the corresponding
+                length.
+            standardize (bool): if true the discriminator will standardize the
+                xdata using the internal method _scale_data.
+            schedules (Union[List[str], List[Schedule]]): The schedules or a
+                subset of schedules in cal_results used to train the
+                discriminator. The user may also pass the name of the schedules
+                instead of the schedules. If schedules is None, then all the
+                schedules in cal_results are used.
+        """
+        self._classifier = classifier
+
+        # Also sets the x and y data.
+        IQDiscriminationFitter.__init__(self, cal_results, qubit_mask,
+                                        expected_states, standardize,
+                                        schedules)
+
+        self._description = (
+            '{} IQ discriminator for measurement level 1.'.format(
+                classifier.__class__.__name__))
+
+        self.fit()
+
+    def fit(self):
+        """ Fits the discriminator using self._xdata and self._ydata. """
+        if len(self._xdata) == 0:
+            return
+
+        self._classifier.fit(self._xdata, self._ydata)
+        self._fitted = True
+
+    def discriminate(self, x_data: List[List[float]]) -> List[str]:
+        """ Applies the discriminator to x_data.
+
+            Args:
+                x_data (List[List[float]]): list of features. Each feature is
+                    itself a list.
+
+            Returns (List[str]):
+                the discriminated x_data as a list of labels.
+        """
+        return self._classifier.predict(x_data)
--- test/measurement/discriminator/test_iq_discriminator.py
+++ test/measurement/discriminator/test_iq_discriminator.py
@@ -24,9 +24,10 @@ import qiskit
 from qiskit import Aer
 from qiskit.ignis.measurement.discriminator.filters import DiscriminationFilter
 from qiskit.ignis.measurement.discriminator.iq_discriminators import \
-    LinearIQDiscriminator
+    LinearIQDiscriminator, SklearnIQDiscriminator
 from qiskit.ignis.mitigation.measurement import circuits
 from qiskit.result.models import ExperimentResultData
+from sklearn.svm import SVC
 
 
 class TestLinearIQDiscriminator(unittest.TestCase):
@@ -169,3 +170,87 @@ class TestLinearIQDiscriminator(unittest.TestCase):
         self.assertFalse(discriminator.is_calibration('cal01101b'))
         self.assertFalse(discriminator.is_calibration('test'))
         self.assertFalse(discriminator.is_calibration('_cal_2121'))
+
+
+class TestSklearnIQDiscriminator(unittest.TestCase):
+    """
+    Test methods of the sklearn IQ discriminators.
+    """
+
+    def setUp(self):
+        """
+        Setup internal variables and a fake simulation. Aer is used to get the
+        structure of the qiskit.Result. The IQ data is generated using gaussian
+        random number generators.
+        """
+        self.shots = 52
+        self.qubits = [0, 1]
+
+        meas_cal, _ = circuits.tensored_meas_cal([[0], [1]])
+
+        backend = Aer.get_backend('qasm_simulator')
+        job = qiskit.execute(meas_cal, backend=backend, shots=self.shots,
+                             meas_level=1)
+
+        self.cal_results = job.result()
+
+        i0, q0, i1, q1 = 0., -1., 0., 1.
+        ground = utils.create_shots(i0, q0, 0.1, 0.1, self.shots, self.qubits)
+        excited = utils.create_shots(i1, q1, 0.1, 0.1, self.shots, self.qubits)
+
+        self.cal_results.results[0].meas_level = 1
+        self.cal_results.results[1].meas_level = 1
+        self.cal_results.results[0].data = ExperimentResultData(memory=ground)
+        self.cal_results.results[1].data = ExperimentResultData(memory=excited)
+
+    def test_discrimination(self):
+        """
+        Test that the discriminator can be trained on the simulated data and
+        that it can properly discriminate between ground and excited sates.
+        """
+        i0, q0, i1, q1 = 0., -1., 0., 1.
+
+        svc = SVC(C=10., kernel="rbg", gamma="scale")
+        discriminator = SklearnIQDiscriminator(
+            svc, self.cal_results, self.qubits, ['00', '11'])
+
+        excited_predicted = discriminator.discriminate([[i1, q1, i1, q1]])
+        ground_predicted = discriminator.discriminate([[i0, q0, i0, q0]])
+
+        self.assertEqual(excited_predicted[0], '11')
+        self.assertEqual(ground_predicted[0], '00')
+
+    def filter_and_discriminate(self):
+        """
+        Test the process of discriminating and then applying the discriminator
+        using a filter.
+        """
+        i0, q0, i1, q1 = 0., -1., 0., 1.
+
+        svc = SVC(C=10., kernel="rbg", gamma="scale")
+        discriminator = SklearnIQDiscriminator(
+            svc, self.cal_results, self.qubits, ['00', '11'])
+
+        iq_filter = DiscriminationFilter(discriminator)
+
+        new_result = iq_filter.apply(self.cal_results)
+
+        for nr in new_result.results:
+            self.assertEqual(nr.meas_level, 2)
+
+        for state in new_result.results[0].data.counts.to_dict():
+            self.assertEqual(state, '0x0')
+
+        for state in new_result.results[1].data.counts.to_dict():
+            self.assertEqual(state, '0x3')
+
+        self.assertEqual(len(new_result.get_memory(0)), self.shots)
+
+        self.qubits = [0]
+
+        svc = SVC(C=10., kernel="rbg", gamma="scale")
+        discriminator = SklearnIQDiscriminator(
+            svc, self.cal_results, self.qubits, ['0', '1'])
+
+        self.assertEqual(discriminator.discriminate([[i0, q0]])[0], '0')
+        self.assertEqual(discriminator.discriminate([[i1, q1]])[0], '1')
