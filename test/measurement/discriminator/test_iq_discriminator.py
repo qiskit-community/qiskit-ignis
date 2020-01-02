ce9d3858d21c4d776e967b98ab881d0958223331
Simon Cross <hodgestar+github@gmail.com>
2020-01-02 18:24:52 +0200

    Add SklearnIQDiscriminator. (#316)
    * Add SklearnIQDiscriminator.
    
    * Fix name of SVC kernel in tests.
    
    * Improve documentration for classifier parameter.
    
    * Factor out base class for IQ discriminator tests.
    
    * Add classifier type checking.
    
    * Add an example showing the use of SVC sklearn discriminators.
    
    * Add docstrings to dummy classifiers.
    
    * Update discriminator tutorial.

--- test/measurement/discriminator/test_iq_discriminator.py
+++ test/measurement/discriminator/test_iq_discriminator.py
@@ -22,16 +22,18 @@ import unittest
 import test.utils as utils
 import qiskit
 from qiskit import Aer
+from qiskit.exceptions import QiskitError
 from qiskit.ignis.measurement.discriminator.filters import DiscriminationFilter
 from qiskit.ignis.measurement.discriminator.iq_discriminators import \
-    LinearIQDiscriminator
+    LinearIQDiscriminator, SklearnIQDiscriminator
 from qiskit.ignis.mitigation.measurement import circuits
 from qiskit.result.models import ExperimentResultData
+from sklearn.svm import SVC
 
 
-class TestLinearIQDiscriminator(unittest.TestCase):
+class BaseTestIQDiscriminator(unittest.TestCase):
     """
-    Test methods of the IQ discriminators.
+    Base class for IQ discriminator test cases.
     """
 
     def setUp(self):
@@ -59,6 +61,12 @@ class TestLinearIQDiscriminator(unittest.TestCase):
         self.cal_results.results[1].meas_level = 1
         self.cal_results.results[0].data = ExperimentResultData(memory=ground)
         self.cal_results.results[1].data = ExperimentResultData(memory=excited)
+
+
+class TestLinearIQDiscriminator(BaseTestIQDiscriminator):
+    """
+    Test methods of the IQ discriminators.
+    """
 
     def test_get_xdata(self):
         """
@@ -169,3 +177,101 @@ class TestLinearIQDiscriminator(unittest.TestCase):
         self.assertFalse(discriminator.is_calibration('cal01101b'))
         self.assertFalse(discriminator.is_calibration('test'))
         self.assertFalse(discriminator.is_calibration('_cal_2121'))
+
+
+class ClassifierWithoutFit:
+    """ A dummy classifier without a .fit() method. """
+    def predict(self):
+        """ A dummy predict method. """
+
+
+class ClassifierWithoutPredict:
+    """ A dummy classifier withou a .predict() method. """
+    def fit(self):
+        """ A dummy fit method. """
+
+
+class TestSklearnIQDiscriminator(BaseTestIQDiscriminator):
+    """
+    Test methods of the sklearn IQ discriminators.
+    """
+
+    def test_classifier_type_check(self):
+        """
+        Test that the discriminator correctly checks that its classifier
+        has fit and predict methods.
+        """
+        with self.assertRaisesRegex(
+                QiskitError,
+                r'^\'Classifier of type "ClassifierWithoutFit" does not have a'
+                r' callable "fit" method\.\'$'
+                ):
+            SklearnIQDiscriminator(
+                ClassifierWithoutFit(), self.cal_results, self.qubits,
+                ['00', '11'])
+
+        with self.assertRaisesRegex(
+                QiskitError,
+                r'^\'Classifier of type "ClassifierWithoutPredict" does not'
+                r' have a callable "predict" method\.\'$'
+                ):
+            SklearnIQDiscriminator(
+                ClassifierWithoutPredict(), self.cal_results, self.qubits,
+                ['00', '11'])
+
+        # check that a valid classifier does not raise an error
+        svc = SVC(C=10., kernel="rbf", gamma="scale")
+        SklearnIQDiscriminator(
+            svc, self.cal_results, self.qubits, ['00', '11'])
+
+    def test_discrimination(self):
+        """
+        Test that the discriminator can be trained on the simulated data and
+        that it can properly discriminate between ground and excited sates.
+        """
+        i0, q0, i1, q1 = 0., -1., 0., 1.
+
+        svc = SVC(C=10., kernel="rbf", gamma="scale")
+        discriminator = SklearnIQDiscriminator(
+            svc, self.cal_results, self.qubits, ['00', '11'])
+
+        excited_predicted = discriminator.discriminate([[i1, q1, i1, q1]])
+        ground_predicted = discriminator.discriminate([[i0, q0, i0, q0]])
+
+        self.assertEqual(excited_predicted[0], '11')
+        self.assertEqual(ground_predicted[0], '00')
+
+    def filter_and_discriminate(self):
+        """
+        Test the process of discriminating and then applying the discriminator
+        using a filter.
+        """
+        i0, q0, i1, q1 = 0., -1., 0., 1.
+
+        svc = SVC(C=10., kernel="rbf", gamma="scale")
+        discriminator = SklearnIQDiscriminator(
+            svc, self.cal_results, self.qubits, ['00', '11'])
+
+        iq_filter = DiscriminationFilter(discriminator)
+
+        new_result = iq_filter.apply(self.cal_results)
+
+        for nr in new_result.results:
+            self.assertEqual(nr.meas_level, 2)
+
+        for state in new_result.results[0].data.counts.to_dict():
+            self.assertEqual(state, '0x0')
+
+        for state in new_result.results[1].data.counts.to_dict():
+            self.assertEqual(state, '0x3')
+
+        self.assertEqual(len(new_result.get_memory(0)), self.shots)
+
+        self.qubits = [0]
+
+        svc = SVC(C=10., kernel="rbf", gamma="scale")
+        discriminator = SklearnIQDiscriminator(
+            svc, self.cal_results, self.qubits, ['0', '1'])
+
+        self.assertEqual(discriminator.discriminate([[i0, q0]])[0], '0')
+        self.assertEqual(discriminator.discriminate([[i1, q1]])[0], '1')
