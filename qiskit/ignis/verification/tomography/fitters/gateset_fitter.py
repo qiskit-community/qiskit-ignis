12b91e6bea79ebc0814f64ad1c5e61bd06d8ebe9
Gadi Aleksandrowicz <gadia@il.ibm.com>
2020-01-05 12:06:47 +0200

    Style changes and packaging

--- qiskit/ignis/verification/tomography/fitters/gateset_fitter.py
+++ qiskit/ignis/verification/tomography/fitters/gateset_fitter.py
@@ -19,71 +19,85 @@ Quantum gate set tomography fitter
 """
 
 import numpy as np
-from qiskit.ignis.verification.tomography.basis.gatesetbasis import StandardGatesetBasis
+from ..basis.gatesetbasis import StandardGatesetBasis
+from .base_fitter import TomographyFitter
 
 
-def compute_probs(data):
-    probs = {}
-    for key in data.keys():
-        vals = data[key]
-        probs[key] = vals.get('0', 0) / sum(vals.values())
-    return probs
+class GatesetTomographyFitter:
+    def __init__(self,
+                 result,
+                 circuits,
+                 gateset_basis='Standard GST'):
+        """Initialize gateset tomography fitter with experimental data.
 
-def gateset_linear_inversion(data, gateset_basis='Standard GST'):
-    """
-    Reconstruct a gate set from measurement data using linear inversion.
+        Args:
+            result (Result): a Qiskit Result object obtained from executing
+                            tomography circuits.
+            circuits (list): a list of circuits or circuit names to extract
+                            count information from the result object.
+            gateset_basis (GateSetBasis, str): Representation of the gates and
+            SPAM circuits of the gateset (default: 'Standard GST')
+        """
+        self.gateset_basis = gateset_basis
+        if gateset_basis == 'Standard GST':
+            self.gateset_basis = StandardGatesetBasis
+        data = TomographyFitter(result, circuits).data
+        self.probs = {}
+        for key, vals in data.items():
+            self.probs[key] = vals.get('0', 0) / sum(vals.values())
 
-    Args:
-        data (vector like): vector of expectation values
-        gateset_basis (String or GateSetBasis): representation of the gateset data
+    def linear_inversion(self):
+        """
+        Reconstruct a gate set from measurement data using linear inversion.
 
-    Returns:
-        A list of tuples (G, label) of gate representation and this gate's label
-        as given in gateset_basis
+        Returns:
+            A list of tuples (G, label) of gate representation
+            and this gate's label as given in gateset_basis
 
-    Additional Information:
-        Given a gate set (G1,..,Gm) and SPAM circuits (F1,...,Fn) constructed from those gates
-        the data should contain the probabilities of the following types:
-        p_{ijk} = <E|F_i G_k F_j |rho>
-        p_{ij} = <E|F_i F_j |rho>
+        Additional Information:
+            Given a gate set (G1,..,Gm)
+            and SPAM circuits (F1,...,Fn) constructed from those gates
+            the data should contain the probabilities of the following types:
+            p_{ijk} = <E|F_i G_k F_j |rho>
+            p_{ij} = <E|F_i F_j |rho>
 
-        One constructs the Gram matrix g = (p_ij)_{ij} which can be described as a product g=AB
-        where A = sum |i><E|F_i| and B=sum |F_j|rho><j|
-        For each gate Gk one can also construct the matrix Mk=(pijk)_{ij}
-        which can be described as Mk=A*Gk*B
-        Inverting g we obtain g^{-1} = B^{-1}A^{-1} and so
-        g^{1} * Mk = B^{-1] * Gk * B
-        This gives us a matrix similiar to Gk's representing matrix.
-        However, it will not be the same as Gk, since the observable results cannot distinguish
-        between (G1,..,Gm) and (B^{-1}*G1*B,..,B^{-1}*Gm*B)
-        a further step of *Gauge optimization* is required on the results
-        of the linear inversion stage. One can also use the linear inversion results
-        as a starting point for a MLE optimization for finding a physical gateset, since
-        unless the probabilities are accurate, the resulting gateset needs not be physical.
-    """
+            One constructs the Gram matrix g = (p_ij)_{ij}
+            which can be described as a product g=AB
+            where A = sum |i><E|F_i| and B=sum |F_j|rho><j|
+            For each gate Gk one can also construct the matrix Mk=(pijk)_{ij}
+            which can be described as Mk=A*Gk*B
+            Inverting g we obtain g^{-1} = B^{-1}A^{-1} and so
+            g^{1} * Mk = B^{-1] * Gk * B
+            This gives us a matrix similiar to Gk's representing matrix.
+            However, it will not be the same as Gk,
+            since the observable results cannot distinguish
+            between (G1,..,Gm) and (B^{-1}*G1*B,..,B^{-1}*Gm*B)
+            a further step of *Gauge optimization* is required on the results
+            of the linear inversion stage.
+            One can also use the linear inversion results as a starting point
+            for a MLE optimization for finding a physical gateset, since
+            unless the probabilities are accurate, the resulting gateset
+            need not be physical.
+        """
 
-    if gateset_basis == 'Standard GST':
-        gateset_basis = StandardGatesetBasis
+        n = len(self.gateset_basis.spam_labels)
+        m = len(self.gateset_basis.gate_labels)
+        gram_matrix = np.zeros((n, n))
+        gate_matrices = []
+        for i in range(m):
+            gate_matrices.append(np.zeros((n, n)))
 
-    probs = compute_probs(data)
-    n = len(gateset_basis.spam_labels)
-    m = len(gateset_basis.gate_labels)
-    gram_matrix = np.zeros((n, n))
-    gate_matrices = []
-    for i in range(m):
-        gate_matrices.append(np.zeros((n, n)))
+        for i in range(n):  # row
+            for j in range(n):  # column
+                F_i = self.gateset_basis.spam_labels[i]
+                F_j = self.gateset_basis.spam_labels[j]
+                gram_matrix[i][j] = self.probs[(F_i, F_j)]
 
-    for i in range(n): # row
-        for j in range(n): # column
-            F_i = gateset_basis.spam_labels[i]
-            F_j = gateset_basis.spam_labels[j]
-            gram_matrix[i][j] = probs[(F_i, F_j)]
+                for k in range(m):  # gate
+                    G_k = self.gateset_basis.gate_labels[k]
+                    gate_matrices[k][i][j] = self.probs[(F_i, G_k, F_j)]
 
-            for k in range(m): #gate
-                G_k = gateset_basis.gate_labels[k]
-                gate_matrices[k][i][j] = probs[(F_i, G_k, F_j)]
+        gram_inverse = np.linalg.inv(gram_matrix)
 
-    gram_inverse = np.linalg.inv(gram_matrix)
-
-    gates = [gram_inverse @ gate_matrix for gate_matrix in gate_matrices]
-    return list(zip(gates, gateset_basis.gate_labels))
+        gates = [gram_inverse @ gate_matrix for gate_matrix in gate_matrices]
+        return list(zip(gates, self.gateset_basis.gate_labels))
