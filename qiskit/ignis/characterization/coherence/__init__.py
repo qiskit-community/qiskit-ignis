8c277064ba1a6be326351cff93b93596f40f7701
yaelbh <yaelbh@il.ibm.com>
2020-01-08 09:36:47 +0200

    lint

--- qiskit/ignis/characterization/coherence/__init__.py
+++ qiskit/ignis/characterization/coherence/__init__.py
@@ -29,16 +29,16 @@ Generation of coherence circuits
 
     num_of_gates = (np.linspace(10, 300, 50)).astype(int)
     gate_time = 0.1
-        
+
     # Note that it is possible to measure several qubits in parallel
     qubits = [0, 2]
-        
+
     t1_circs, t1_xdata = t1_circuits(num_of_gates, gate_time, qubits)
     t2star_circs, t2star_xdata, osc_freq = t2star_circuits(num_of_gates, gate_time, qubits, nosc=5)
-    t2echo_circs, t2echo_xdata = t2_circuits(np.floor(num_of_gates/2).astype(int), 
+    t2echo_circs, t2echo_xdata = t2_circuits(np.floor(num_of_gates/2).astype(int),
                                              gate_time, qubits)
-    t2cpmg_circs, t2cpmg_xdata = t2_circuits(np.floor(num_of_gates/6).astype(int), 
-                                             gate_time, qubits, 
+    t2cpmg_circs, t2cpmg_xdata = t2_circuits(np.floor(num_of_gates/6).astype(int),
+                                             gate_time, qubits,
                                              n_echos=5, phase_alt_echo=True)
 
 Backend execution
@@ -60,29 +60,32 @@ Backend execution
     # Define T1 and T2 noise:
     t1_noise_model = NoiseModel()
     t1_noise_model.add_quantum_error(
-    thermal_relaxation_error(t_q0, 2*t_q0, gate_time), 
+    thermal_relaxation_error(t_q0, 2*t_q0, gate_time),
                             'id', [0])
     t1_noise_model.add_quantum_error(
-        thermal_relaxation_error(t_q2, 2*t_q2, gate_time), 
+        thermal_relaxation_error(t_q2, 2*t_q2, gate_time),
         'id', [2])
 
     t2_noise_model = NoiseModel()
     t2_noise_model.add_quantum_error(
-    thermal_relaxation_error(np.inf, t_q0, gate_time, 0.5), 
+    thermal_relaxation_error(np.inf, t_q0, gate_time, 0.5),
         'id', [0])
     t2_noise_model.add_quantum_error(
-        thermal_relaxation_error(np.inf, t_q2, gate_time, 0.5), 
+        thermal_relaxation_error(np.inf, t_q2, gate_time, 0.5),
         'id', [2])
 
     # Run the simulator
     t1_backend_result = qiskit.execute(t1_circs, backend, shots=shots,
                                        noise_model=t1_noise_model, optimization_level=0).result()
     t2star_backend_result = qiskit.execute(t2star_circs, backend, shots=shots,
-                                           noise_model=t2_noise_model, optimization_level=0).result()
+                                           noise_model=t2_noise_model,
+                                           optimization_level=0).result()
     t2echo_backend_result = qiskit.execute(t2echo_circs, backend, shots=shots,
-                                           noise_model=t2_noise_model, optimization_level=0).result()
+                                           noise_model=t2_noise_model,
+                                           optimization_level=0).result()
 
-    # It is possible to split the circuits into multiple jobs and then give the results to the fitter as a list:
+    # It is possible to split the circuits into multiple jobs and then give the results
+    # to the fitter as a list:
     t2cpmg_backend_result1 = qiskit.execute(t2cpmg_circs[0:5], backend,
                                             shots=shots, noise_model=t2_noise_model,
                                             optimization_level=0).result()
@@ -127,7 +130,7 @@ Analysis of results
         plt.figure(figsize=(15, 6))
         for i in range(2):
             ax = plt.subplot(1, 2, i+1)
-            t1_fit.plot(i, ax=ax)    
+            t1_fit.plot(i, ax=ax)
         plt.show()
 
     Fitting T2-star
@@ -144,13 +147,13 @@ Analysis of results
        plt.figure(figsize=(15, 6))
        for i in range(2):
             ax = plt.subplot(1, 2, i+1)
-            t2star_fit.plot(i, ax=ax)    
+            t2star_fit.plot(i, ax=ax)
        plt.show()
 
     Fitting T2 single echo
 
     .. jupyter-execute::
-        
+
        from qiskit.ignis.characterization.coherence import T2Fitter
 
        t2echo_fit = T2Fitter(t2echo_backend_result, t2echo_xdata, qubits,
@@ -163,11 +166,11 @@ Analysis of results
        plt.figure(figsize=(15, 6))
        for i in range(2):
            ax = plt.subplot(1, 2, i+1)
-           t2echo_fit.plot(i, ax=ax)    
+           t2echo_fit.plot(i, ax=ax)
        plt.show()
 
     Fitting T2 CPMG
-    
+
     .. jupyter-execute::
 
         t2cpmg_fit = T2Fitter([t2cpmg_backend_result1, t2cpmg_backend_result2],
