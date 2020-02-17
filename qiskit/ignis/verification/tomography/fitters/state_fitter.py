1060ce0ad9adbacc561004063bce0d8bebbaf0d2
gadial <gadial@gmail.com>
2020-02-17 10:58:49 +0200

    State fitter docs

--- qiskit/ignis/verification/tomography/fitters/state_fitter.py
+++ qiskit/ignis/verification/tomography/fitters/state_fitter.py
@@ -15,7 +15,11 @@
 
 """Maximum-Likelihood estimation quantum state tomography fitter
 """
-
+from typing import List, Union
+import numpy as np
+from qiskit.result import Result
+from qiskit import QuantumCircuit
+from ..basis import TomographyBasis
 from .base_fitter import TomographyFitter
 
 
@@ -23,23 +27,28 @@ class StateTomographyFitter(TomographyFitter):
     """Maximum-Likelihood estimation state tomography fitter."""
 
     def __init__(self,
-                 result,
-                 circuits,
-                 meas_basis='Pauli'):
+                 result: Result,
+                 circuits: List[QuantumCircuit],
+                 meas_basis: Union[TomographyBasis, str]='Pauli'
+                 ):
         """Initialize state tomography fitter with experimental data.
 
         Args:
-            result (Result): a Qiskit Result object obtained from executing
+            result: a Qiskit Result object obtained from executing
                 tomography circuits.
-            circuits (list): a list of circuits or circuit names to extract
+            circuits: a list of circuits or circuit names to extract
                 count information from the result object.
-            meas_basis (TomographyBasis, str): A function to return measurement
+            meas_basis: A function to return measurement
                 operators corresponding to measurement outcomes. See
                 Additional Information (default: 'Pauli')
         """
         super().__init__(result, circuits, meas_basis, None)
 
-    def fit(self, method='auto', standard_weights=True, beta=0.5, **kwargs):
+    def fit(self,
+            method: str='auto',
+            standard_weights: bool=True,
+            beta: float=0.5,
+            **kwargs) -> np.array:
         r"""Reconstruct a quantum state using CVXPY convex optimization.
         .. jupyter-execute::
 
@@ -100,14 +109,12 @@ class StateTomographyFitter(TomographyFitter):
             (2012). Open access: arXiv:1106.5458 [quant-ph].
 
         Args:
-            method (str): The fitter method 'auto', 'cvx' or 'lstsq'.
-            standard_weights (bool, optional): Apply weights to
+            method: The fitter method 'auto', 'cvx' or 'lstsq'.
+            standard_weights: Apply weights to
                 tomography data based on count probability
-                (default: True)
-            beta (float): hedging parameter for converting counts
+            beta: hedging parameter for converting counts
                 to probabilities
-                (default: 0.5)
-            **kwargs (optional): kwargs for fitter method.
+            **kwargs: kwargs for fitter method.
 
         Returns:
             The fitted matrix rho that minimizes
