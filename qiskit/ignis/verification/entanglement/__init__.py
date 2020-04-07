aaf13515d5fb27cfcf4e5d033018ff896fa449ec
Yehuda Naveh <naveh@il.ibm.com>
2020-04-07 23:22:38 +0300

    imports in __init__.py

--- qiskit/ignis/verification/entanglement/__init__.py
+++ qiskit/ignis/verification/entanglement/__init__.py
@@ -1,7 +1,7 @@
 '''
 In this package, there are three modules, linear.py, parallellize.py,
 and analysis.py. Once Terra completes design for circuit repository,
-may be more suitable to move it there. The most important module,
+it may be more suitable to move it there. The most important module,
 parallellize.py provides methods to parallellize CNOT gates in the
 preparation of the GHZ State, which results to the GHZ State having a
 much higher fidelity then a normal "linear" CNOT gate preparation of
@@ -13,3 +13,18 @@ the linear preparation analogous of parallelize.py. The module
 analysis.py provides several miscellaneous tools for analysis of the
 GHZ State (most notably, Fourier Analysis)
 '''
+
+# Entanglement functions
+from .analysis import ordered_list_generator
+from .analysis import composite_pauli_z
+from .analysis import composite_pauli_z_expvalue
+from .analysis import Plotter
+from .analysis import rho_to_fidelity
+from .linear import get_measurement_circ
+from .linear import get_ghz_simple
+from .linear import get_ghz_mqc
+from .linear import get_ghz_mqc_para
+from .linear import get_ghz_po
+from .linear import get_ghz_po_para
+from .parallelize import BConfig
+from .fitters import QVFitter
