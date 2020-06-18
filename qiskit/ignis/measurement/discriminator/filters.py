08ffae0dde6a499adbd3e4d2bd9deaf15c816298
Matthew Treinish <mtreinish@kortar.org>
2020-06-18 16:09:01 -0400

    Revert accidental breaking backports (#427)
    * Revert #400/#401 from stable branch
    
    The change made in #400 (which was missed because it got integrated into
    the #401 PR which was needed to unblock CI on master) should not have
    been backported. It was only necessary for compat with terra master and
    terra 0.15.0 because the validation module is removed in 0.15.0. This is
    breaking measurement mitigation on the stable branch and 0.3.1 release
    which needs to work with terra 0.14.x. This commit reverts that change
    so the we're wrapping the counts in qiskit.validation.Obj and the
    Results object is useable in marshmallow with released terra.

--- qiskit/ignis/measurement/discriminator/filters.py
+++ qiskit/ignis/measurement/discriminator/filters.py
@@ -26,6 +26,7 @@ from qiskit.ignis.measurement.discriminator.discriminators import \
     BaseDiscriminationFitter
 from qiskit.result.result import Result
 from qiskit.result.models import ExperimentResultData
+from qiskit.validation.base import Obj
 
 
 class DiscriminationFilter:
@@ -87,7 +88,7 @@ class DiscriminationFilter:
         start = 0
         for idx, n_shots in enumerate(shots_per_experiment_result):
             memory = y_data[start:(start+n_shots)]
-            counts = self.count(memory)
+            counts = Obj.from_dict(self.count(memory))
             new_results.results[idx].data = ExperimentResultData(counts=counts,
                                                                  memory=memory)
             start += n_shots
