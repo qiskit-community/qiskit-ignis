0f87ae5b27c1ee5226c91a1f731764556c6e5be8
Christopher Wood <cjwood@us.ibm.com>
2020-07-28 16:15:44 -0400

    Add assignment fidelity and plotting function to mitigator classes

--- qiskit/ignis/mitigation/measurement/base_meas_mitigator.py
+++ qiskit/ignis/mitigation/measurement/base_meas_mitigator.py
@@ -19,6 +19,12 @@ from abc import ABC, abstractmethod
 from typing import Optional, List, Dict
 import numpy as np
 
+try:
+    import matplotlib.pyplot as plt
+    _HAS_MATPLOTLIB = True
+except ImportError:
+    _HAS_MATPLOTLIB = False
+
 
 class BaseMeasMitigator(ABC):
     """Base measurement error mitigator class."""
@@ -57,8 +63,53 @@ class BaseMeasMitigator(ABC):
         """
 
     @abstractmethod
+    def mitigation_matrix(self, qubits: List[int] = None) -> np.ndarray:
+        r"""Return the measurement mitigation matrix for the specified qubits.
+
+        The mitigation matrix :math:`A^{-1}` is defined as the inverse of the
+        :meth:`assignment_matrix` :math:`A`.
+
+        Args:
+            qubits: Optional, qubits being measured for operator expval.
+
+        Returns:
+            np.ndarray: the measurement error mitigation matrix :math:`A^{-1}`.
+        """
+
+    @abstractmethod
+    def assignment_matrix(self, qubits: List[int] = None) -> np.ndarray:
+        r"""Return the measurement assignment matrix for specified qubits.
+
+        The assignment matrix is the stochastic matrix :math:`A` which assigns
+        a noisy measurement probability distribution to an ideal input
+        measurement distribution: :math:`P(i|j) = \langle i | A | j \rangle`.
+
+        Args:
+            qubits: Optional, qubits being measured for operator expval.
+
+        Returns:
+            np.ndarray: the assignment matrix A.
+        """
+
+    @abstractmethod
     def _compute_gamma(self, qubits: Optional[List[int]] = None) -> float:
         """Compute gamma for N-qubit mitigation."""
+
+    def assignment_fidelity(self, qubits: Optional[List[int]] = None) -> float:
+        r"""Return the measurement assignment fidelity on the specified qubits.
+
+        The assignment fidelity on N-qubits is defined as
+        :math:`\sum_{x\in\{0, 1\}^n} P(x|x) / 2^n`, where
+        :math:`P(x|x) = \rangle x|A|x\langle`, and :math:`A` is the
+        :meth:`assignment_matrix`.
+
+        Args:
+            qubits: Optional, qubits being measured for operator expval.
+
+        Returns:
+            float: the assignment fidelity.
+        """
+        return self.assignment_matrix(qubits=qubits).diagonal().mean()
 
     def required_shots(self, delta: float, qubits: Optional[List[int]] = None) -> int:
         r"""Return the number of shots required for expectation value estimation.
@@ -108,9 +159,107 @@ class BaseMeasMitigator(ABC):
         gamma = self._compute_gamma(qubits=qubits)
         return gamma / np.sqrt(shots)
 
+    def plot_assignment_matrix(self,
+                               qubits: Optional[List[int]] = None,
+                               ax: Optional[plt.axes] = None) -> plt.axes:
+        """Matrix plot of the readout error assignment matrix.
+
+        Args:
+            qubits: Optional, qubits being measured for operator expval.
+            ax: Optional. Axes object to add plot to.
+
+        Returns:
+            plt.axes: the figure axes object.
+
+        Raises:
+            ImportError: if matplotlib is not installed.
+        """
+        if not _HAS_MATPLOTLIB:
+            raise ImportError(
+                'Plotting functions require the optional matplotlib package.'
+                ' Install with `pip install matplotlib`.')
+
+        mat = self.assignment_matrix(qubits)
+        if ax is None:
+            figure = plt.figure()
+            ax = figure.gca()
+        axim = ax.matshow(mat, cmap=plt.cm.binary, clim=[0, 1])
+        ax.figure.colorbar(axim)
+        ax = self._plot_axis(mat, ax=ax)
+        return ax
+
+    def plot_mitigation_matrix(self,
+                               qubits: Optional[List[int]] = None,
+                               ax: Optional[plt.axes] = None) -> plt.axes:
+        """Matrix plot of the readout error mitigation matrix.
+
+        Args:
+            qubits: Optional, qubits being measured for operator expval.
+            ax: Optional. Axes object to add plot to.
+
+        Returns:
+            plt.axes: the figure axes object.
+
+        Raises:
+            ImportError: if matplotlib is not installed.
+        """
+        if not _HAS_MATPLOTLIB:
+            raise ImportError(
+                'Plotting functions require the optional matplotlib package.'
+                ' Install with `pip install matplotlib`.')
+
+        # Matrix parameters
+        mat = self.mitigation_matrix(qubits)
+        mat_max = np.max(mat)
+        mat_min = np.min(mat)
+        lim = max(abs(mat_max), abs(mat_min), 1)
+
+        if ax is None:
+            figure = plt.figure()
+            ax = figure.gca()
+        axim = ax.matshow(mat, cmap=plt.cm.RdGy, clim=[-lim, lim])
+        ax.figure.colorbar(axim, values=np.linspace(mat_min, mat_max, 100))
+        ax = self._plot_axis(mat, ax=ax)
+        return ax
+
     @staticmethod
     def _z_diagonal(dim):
+        r"""Return the diagonal for the operator :math:`Z^\otimes n`"""
         parity = np.zeros(dim, dtype=np.int)
         for i in range(dim):
             parity[i] = bin(i)[2:].count('1')
         return (-1)**np.mod(parity, 2)
+
+    @staticmethod
+    def _int_to_bitstring(i, num_qubits=None):
+        """Convert an integer to a bitstring."""
+        label = bin(i)[2:]
+        if num_qubits:
+            pad = num_qubits - len(label)
+            if pad > 0:
+                label = pad * '0' + label
+        return label
+
+    @staticmethod
+    def _plot_axis(mat: np.ndarray, ax: plt.axes) -> plt.axes:
+        """Helper function for setting up axes for plots.
+
+        Args:
+            mat: the N-qubit matrix to plot.
+            ax: Optional. Axes object to add plot to.
+
+        Returns:
+            plt.axes: the figure object and axes object.
+        """
+        dim = len(mat)
+        num_qubits = int(np.log2(dim))
+        bit_labels = [BaseMeasMitigator._int_to_bitstring(i, num_qubits) for i in range(dim)]
+
+        ax.set_xticks(np.arange(dim))
+        ax.set_yticks(np.arange(dim))
+        ax.set_xticklabels(bit_labels, rotation=90)
+        ax.set_yticklabels(bit_labels)
+        ax.set_xlabel('Prepared State')
+        ax.xaxis.set_label_position('top')
+        ax.set_ylabel('Measured State')
+        return ax
