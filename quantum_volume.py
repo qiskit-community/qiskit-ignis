6b5ab06416163afc85faa11fcaff28c4d1ee85ea
dsche <dscheiermann97@gmail.com>
2020-10-08 17:28:36 +0200

    Adds copy of nb

---
+++ quantum_volume.py
@@ -0,0 +1,90 @@
+import matplotlib.pyplot as plt
+
+#Import Qiskit classes
+import qiskit
+from qiskit.providers.aer.noise import NoiseModel
+from qiskit.providers.aer.noise.errors.standard_errors import depolarizing_error, thermal_relaxation_error
+
+#Import the qv function
+import qiskit.ignis.verification.quantum_volume as qv
+
+# qubit_lists: list of list of qubit subsets to generate QV circuits
+qubit_lists = [[0,1,3],[0,1,3,5],[0,1,3,5,7],[0,1,3,5,7,10]]
+# ntrials: Number of random circuits to create for each subset
+ntrials = 50
+
+qv_circs, qv_circs_nomeas = qv.qv_circuits(qubit_lists, ntrials)
+
+#pass the first trial of the nomeas through the transpiler to illustrate the circuit
+qv_circs_nomeas[0] = qiskit.compiler.transpile(qv_circs_nomeas[0], basis_gates=['u1','u2','u3','cx'])
+
+#The Unitary is an identity (with a global phase)
+backend = qiskit.Aer.get_backend('statevector_simulator')
+ideal_results = []
+for trial in range(ntrials):
+    print('Simulating trial %d'%trial)
+    ideal_results.append(qiskit.execute(qv_circs_nomeas[trial], backend=backend).result())
+
+qv_fitter = qv.QVFitter(qubit_lists=qubit_lists)
+qv_fitter.add_statevectors(ideal_results)
+
+for qubit_list in qubit_lists:
+    l = len(qubit_list)
+    print ('qv_depth_'+str(l)+'_trial_0:', qv_fitter._heavy_outputs['qv_depth_'+str(l)+'_trial_0'])
+
+for qubit_list in qubit_lists:
+    l = len(qubit_list)
+    print ('qv_depth_'+str(l)+'_trial_0:', qv_fitter._heavy_output_prob_ideal['qv_depth_'+str(l)+'_trial_0'])
+
+noise_model = NoiseModel()
+p1Q = 0.002
+p2Q = 0.02
+noise_model.add_all_qubit_quantum_error(depolarizing_error(p1Q, 1), 'u2')
+noise_model.add_all_qubit_quantum_error(depolarizing_error(2*p1Q, 1), 'u3')
+noise_model.add_all_qubit_quantum_error(depolarizing_error(p2Q, 2), 'cx')
+#noise_model = None
+
+backend = qiskit.Aer.get_backend('qasm_simulator')
+basis_gates = ['u1','u2','u3','cx'] # use U,CX for now
+shots = 1024
+exp_results = []
+for trial in range(ntrials):
+    print('Running trial %d'%trial)
+    exp_results.append(qiskit.execute(qv_circs[trial], basis_gates=basis_gates, backend=backend, noise_model=noise_model, backend_options={'max_parallel_experiments': 0}).result())
+
+qv_fitter.add_data(exp_results)
+for qubit_list in qubit_lists:
+    l = len(qubit_list)
+    #print (qv_fitter._heavy_output_counts)
+    print ('qv_depth_'+str(l)+'_trial_0:', qv_fitter._heavy_output_counts['qv_depth_'+str(l)+'_trial_0'])
+
+plt.figure(figsize=(10, 6))
+ax = plt.gca()
+
+# Plot the essence by calling plot_rb_data
+qv_fitter.plot_qv_data(ax=ax, show_plt=False)
+
+# Add title and label
+ax.set_title('Quantum Volume for up to %d Qubits \n and %d Trials'%(len(qubit_lists[-1]), ntrials), fontsize=18)
+
+plt.show()
+
+print(qv_list)
+
+qv_success_list = qv_fitter.qv_success()
+qv_list = qv_fitter.ydata
+QV = 1
+my_trial = 0
+for qidx, qubit_list in enumerate(qubit_lists):
+    if qv_list[my_trial][qidx]>2/3:
+        if qv_success_list[qidx][my_trial]:
+            print("Width/depth %d greater than 2/3 (%f) with confidence %f (successful). Quantum volume %d"%
+                  (len(qubit_list),qv_list[0][qidx],qv_success_list[qidx][1],qv_fitter.quantum_volume()[qidx]))
+            QV = qv_fitter.quantum_volume()[qidx]
+        else:
+            print("Width/depth %d greater than 2/3 (%f) with confidence %f (unsuccessful)."%
+                  (len(qubit_list),qv_list[0][qidx],qv_success_list[qidx][1]))
+    else:
+        print("Width/depth %d less than 2/3 (unsuccessful)."%len(qubit_list))
+
+print ("The Quantum Volume is:", QV)
