2c0a99611908f379ec7fbd1dde17f4503ebfd489
Tharrmashastha SAPV <51048173+TharrmashasthaPV@users.noreply.github.com>
2020-10-09 19:14:44 +0530

    Push circuits.py and quantum_volume_calculator.py

--- qiskit/ignis/verification/quantum_volume/circuits.py
+++ qiskit/ignis/verification/quantum_volume/circuits.py
@@ -108,55 +108,45 @@ def qv_circuits(qubit_lists, ntrials=1,
 
 
 def qv_circuits_opt(qubit_lists=None, ntrials=1, max_qubits=2,
-                backend=None, qr=None, cr=None, seed=None, n_desired_layouts=1, nth_best=0):
+                backend=None, qr=None, cr=None, seed=None):
     """
     Return a list of quantum volume circuits transpiled on
-    for specific backend. The circuit will be square (depth=width)
-    as long as the user's specified layout doesn't require extra
-    qubits to perform any swaps needed. If  no user-specified layout is
-    provided, the function will try to find the best suitable qubtis.
+     for specific backend. The circuit will be square (depth=width)
+     as long as the user's specified layout doesn't require extra
+     qubits to perform any swaps needed. If  no user-specified layout is
+     provided, the function will try to find the best suitable qubtis.
 
     Args:
         qubit_lists (list): list of list of qubits to apply qv circuits to. Assume
             the list is ordered in increasing number of qubits
-
         ntrials (int): number of random iterations
-
         qr (QuantumRegister): quantum register to act on (if None one is created)
-
         cr (ClassicalRegister): classical register to measure to (if None one is created)
-
         seed (int): An optional RNG seed to use for the generated circuit
-
-        max_qubits (int): Will be used if the user doesn't specify their desired layout.
-            Minimum value is 2
-
+        max_qubits (int): Will be used if the user doesn't specify their desired layout. Minimum value is 2
         backend (IBMQBackend): A backend for the quantum volume circuits to be transpiled
-            for.
-
-        n_desired_layouts (int): Number of layouts chosen (with most occurencies)
-
-        nth_best (int): Choose the n-th best layout of the n_desired_layouts,
-            so n_desired_layouts > nth_best
+        for.
 
     Returns:
         tuple: A tuple of the type (``circuits``, ``circuits_nomeas``) wheere:
             ``circuits`` is a list of lists of circuits for the qv sequences
             (separate list for each trial) and `` circuitss_nomeas`` is the
             same circuits but with no measurements for the ideal simulation
-    """     
-    assert (n_desired_layouts > nth_best) , "Error: It needs to be n_desired_layouts > nth_best "
+    """
+    if backend.configuration().simulator:
+        return qv_circuits(qubit_lists, ntrials=ntrials)
 
     if seed:
         rng = np.random.default_rng(seed)
     else:
         _seed = None
-
     
     qv_circs = [[] for e in range(ntrials)]
     circuits_nomeas = [[] for e in range(ntrials)]
 
+    ql_cond = True
     if qubit_lists == None:
+        ql_cond = False
         depth_list = [i for i in range(2,max_qubits+1)]
 
     else:
@@ -182,15 +172,15 @@ def qv_circuits_opt(qubit_lists=None, ntrials=1, max_qubits=2,
 
     if qubit_lists == None:
         # find layouts for a range of qubits from 2 up to max_qubits
-        best_layouts_list = [[] for tmp in range(max_qubits-2)]
-        for n_qubits in range(3, max_qubits+1, 1):
-            best_layouts_list[n_qubits-3] = get_layout(qv_circs, n_qubits, ntrials, backend,
-                transpile_trials=None, n_desired_layouts=n_desired_layouts)
+        best_layouts_list = [[] for tmp in range(max_qubits-1)]
+        for n_qubits in range(2, max_qubits+1, 1):
+            best_layouts_list[n_qubits-2] = get_layout(qv_circs, n_qubits, ntrials, backend,
+                                                          transpile_trials=None, n_desired_layouts=1)
         # [[n_desired_layouts * Layouts], [n_desired_layouts * Layouts], [], [], [], []]
         qubit_lists = []
 
         for good_layout in best_layouts_list:
-            qubit_lists.append(good_layout[nth_best])
+            qubit_lists.append(good_layout[0])
 
     else:
         warnings.warn("The choice of the qubit list may result in extra swaps and extra qubits"
@@ -209,7 +199,10 @@ def qv_circuits_opt(qubit_lists=None, ntrials=1, max_qubits=2,
 
             circuits[trial].append(qc)
 
-    return circuits, circuits_nomeas
+    if ql_cond==False:
+        return circuits, circuits_nomeas, qubit_lists
+    else:
+        return circuits, circuits_nomeas
 
 
 def get_layout(qv_circs, n_qubits, n_trials, backend, transpile_trials=None, n_desired_layouts=1):
@@ -255,46 +248,6 @@ def get_layout(qv_circs, n_qubits, n_trials, backend, transpile_trials=None, n_d
 
     return sorted_layouts[:n_desired_layouts]
 
-
-def get_layout_low_cost(num_qubits, backend, layout_method='noise_adaptive'):
-    """     
-    Creates a mock circuit similar to the SU(4) circuit
-    to be used with the transpiler allocation passes.
-
-    returns the initial_layour
-    """     
-    if (num_qubits > backend.configuration().n_qubits): 
-        return None     
-    
-    cct = QuantumCircuit(num_qubits)     
-    comb = combinations(range(num_qubits), 2)    
-
-    for i in comb:
-        for j in range(3):
-            cct.cx(i[0],i[1]) 
-            cct.u3(np.pi/2, np.pi/2,np.pi/2, i[0])
-            cct.u3(np.pi/2, np.pi/2,np.pi/2, i[1])         
- 
-    cct_meas = copy.deepcopy(cct)
-    cct_meas.measure_active()
-
-
-    pm = PassManager()
-    pm.append(Unroll3qOrMore())
-    
-    if layout_method == 'noise_adaptive':
-        pm.append(NoiseAdaptiveLayout(backend.properties()))
-    elif layout_method == 'dense':
-        pm.append(DenseLayout(CouplingMap(backend.configuration().coupling_map), backend.properties()))
-    elif layout_method == 'sabre':
-        pm.append(SabreLayout(CouplingMap(backend.configuration().coupling_map)))
-
-    
-    pm.run(cct_meas)
-    layout = list(pm.property_set['layout'].get_physical_bits().keys())
-
-    return layout
-
 if __name__ == "__main__":
     qubit_lists = [[0,1,3], [0,1,3,5], [0,1,3,5,7], [0,1,3,5,7,10]]
     # ntrials: Number of random circuits to create for each subset
@@ -303,6 +256,6 @@ if __name__ == "__main__":
     fake_backend = FakeMelbourne()
 
     qv_circs, qv_circs_nomeas = qv_circuits_opt(ntrials=ntrials, max_qubits=4,
-        backend=fake_backend, qr=None, cr=None, seed=None, n_desired_layouts=6, nth_best=5)
+                    backend=fake_backend)
     print("qv_circ[0]: ", qv_circs[0])
     print(qv_circs[0][0])
--- qiskit/ignis/verification/quantum_volume/quantum_volume_calculator.py
+++ qiskit/ignis/verification/quantum_volume/quantum_volume_calculator.py
@@ -25,6 +25,7 @@ import numpy as np
 from qiskit import QiskitError
 from qiskit.ignis.verification.quantum_volume import circuits
 from qiskit.ignis.verification.quantum_volume import fitters, QVFitter
+import qiskit.ignis.verification.quantum_volume as qv
 import qiskit.execute
 from qiskit.circuit.quantumcircuit import QuantumCircuit
 import qiskit.providers.aer
@@ -37,24 +38,31 @@ import time
 class quantum_volume:
 	def __init__(self, backend, ntrials=50, qubit_list=None, noise_model=None, basis_gates=None):
 		self._noise_model=None
+
 		if 'qasm' in backend.name():
 			if noise_model == None:
 				raise QiskitError("qasm-simulator backends requires a noise model")
+			elif qubit_list == None:
+				raise QiskitError("Simulators need qubit lists.")
 			else:
 				self._noise_model = noise_model
-		
-		if qubit_list == None:
-			raise QiskitError("WIP: Please input a list of qubits to run the quantum volume calculation on")
-		else:
-			self._qubit_list = qubit_list
+
+		self._qubit_list = qubit_list
 
 		self._basis_gates = basis_gates
 		self._backend = backend
 		self._is_simulator = backend.configuration().simulator
+		if 'fake' in backend.configuration().backend_name:
+			self._is_simulator = True    
     
 		self._ntrials = ntrials
-		self._max_depth = len(qubit_list)
-		self._list_of_qubit_lists = self.generate_qubit_lists()
+		self._qlist_cond = False
+		if self._qubit_list:
+			self._max_depth = len(qubit_list)
+			self._list_of_qubit_lists = self.generate_qubit_lists()
+			self._qlist_cond = True
+		else:
+			self._max_depth = self._backend.configuration().num_qubits
 		self._simulator_backend = qiskit.Aer.get_backend('statevector_simulator')
 
 	#TODO: This is a simple approach. I am just generating, from [0,1,2,3,4,5,6], [[0,1],[0,1,2].[0,1,2,3],[0,1,2,3,4],[0,1,2,3,4,5],[0,1,2,3,4,5,6]]
@@ -71,52 +79,61 @@ class quantum_volume:
 		#Creates a qubit list for each depth to be tested, up to the maxiumum depth == len(qubit_list)
 		return [self._qubit_list[:depth] for depth in range(3, self._max_depth + 1)]
 
-	def generate_list_of_trails(self, depth):
-		return(circuits.qv_circuits([self._list_of_qubit_lists[depth - 3]], self._ntrials)) #The -2 corrects the depth -> index
-
+	def generate_list_of_trails(self,depth):
+		if self._qlist_cond:
+			y = [self._list_of_qubit_lists[depth - 3]]
+			return circuits.qv_circuits_opt(y,
+			 ntrials=self._ntrials, max_qubits=self._max_depth, backend=self._backend) #The -2 corrects the depth -> index
+		else:
+			return circuits.qv_circuits_opt(ntrials = self._ntrials, max_qubits=self._max_depth, backend=self._backend)
 
 	def find_quantum_volume(self):
 		managed_job_list = []
 		sim_results_list = []
-		
-		for depth in range(3, self._max_depth + 1): #Don't check depth == 1
+		if not self._qlist_cond:
+			qv_circs_real, qv_circs_sim, self._list_of_qubit_lists = self.generate_list_of_trails(depth=None)
+		for depth in range(3, self._max_depth + 1): #Don't check if depth == 1 or 2
 			sim_results = []
 			real_results = []
-			qv_circs_real, qv_circs_sim = self.generate_list_of_trails(depth) #Creates one list of circuits to run and one to simulate. 
+			if self._qlist_cond:
+				qv_circs_real, qv_circs_sim = self.generate_list_of_trails(depth=depth) #Creates one list of circuits to run and one to simulate.
 			#Simulates results stored in a list of results
 			for trial in range(self._ntrials):
-				sim_results.append(qiskit.execute(qv_circs_sim[trial], backend=self._simulator_backend).result())
+				if self._qlist_cond:
+					sim_results.append(qiskit.execute(qv_circs_sim[trial], backend=self._simulator_backend).result())
+				else:
+					sim_results.append(qiskit.execute(qv_circs_sim[trial][depth-3], backend=self._simulator_backend).result())
 			sim_results_list.append(sim_results)
-			# qv_circs_sim = [circ[0] for circ in qv_circs_sim]
-			# sim_results = execute(qv_circs_sim, backend=self._simulator_backend).result()
 			#Create the job manager for the depth run
 			job_manager = IBMQJobManager()
 			#Strips the outer list from qv_circs_real
-			qv_circs_real = [circ[0] for circ in qv_circs_real]
+			if self._qlist_cond:
+				qv_circs_real_temp = [circ[0] for circ in qv_circs_real]
+			else:
+				qv_circs_real_temp = [circ[depth-3] for circ in qv_circs_real]
+			init_layout = list(qv_circs_real_temp[0]._layout.get_physical_bits().keys())
 			if self._is_simulator:
-				managed_job = execute(qv_circs_real, backend = self._backend, noise_model = self._noise_model ,basis_gates=self._basis_gates)
+				managed_job = execute(qv_circs_real_temp, backend = self._backend ,basis_gates=self._basis_gates, initial_layout = init_layout)
 			else:
 				#Transpile circuits, and create a ManagedJobSet object to later call the results, then stores it in a list
-				qv_circs_real = transpile(qv_circs_real, backend = self._backend)
-				managed_job = job_manager.run(qv_circs_real, backend = self._backend)
+				qv_circs_real = transpile(qv_circs_real_temp, backend = self._backend, initial_layout = init_layout)
+				managed_job = job_manager.run(qv_circs_real_temp, backend = self._backend, initial_layout = init_layout)
 			managed_job_list.append(managed_job)
 		if self._is_simulator:
 			QV = self.find_best_qv_sim(managed_job_list,sim_results_list)
 		else:
 			QV = self.find_best_qv_real(managed_job_list,sim_results_list)
 		return QV
-		
+
 		#For each managed job in the list, try to call the results. Once it gets one result, it processes it, then determines if the rest of the 
 		#results should continue to run or be stopped before running to save resources. If the QV calc fails, there is no point in running higher 
 		#width circuits
 	def find_best_qv_real(self,managed_job_list,sim_results_list):
 		current_best_qv = 1
 		for managed_job_index in range(len(managed_job_list)):
-
 			sim_results = sim_results_list[managed_job_index]
 			waiting = True
 			#While the results are pending, keep looping and waiting. Break the loop and return the result once the job is finished
-
 			print("Began requesting results")
 			while waiting:
 				print(managed_job_list[managed_job_index].statuses()[-1])
@@ -129,9 +146,7 @@ class quantum_volume:
 			print("Successfully retrieved Results for depth = " + str(managed_job_index + 3))
 			
 			current_list = self._list_of_qubit_lists[managed_job_index]
-			success, qv_fitter  = self.qvolume(current_list,sim_results,real_results)
-			# qv_fitter.plot_qv_data()
-			# print(success)
+			success, qv_fitter  = self.qvolume(current_list,sim_results,repal_results)
 			#Determine if the QV calculation completed. If it did, store the QV and continue running.  If not, then cancel the rest of the 
 			#Jobs and return the previous best
 			if success:
@@ -151,8 +166,6 @@ class quantum_volume:
 			real_results = managed_job_list[managed_job_index].result()
 			current_list = self._list_of_qubit_lists[managed_job_index]
 			success, qv_fitter  = self.qvolume(current_list,sim_results,real_results)
-			# qv_fitter.plot_qv_data()
-			# print(success)
 			#Determine if the QV calculation completed. If it did, store the QV and continue running.  If not, then cancel the rest of the 
 			#Jobs and return the previous best
 			if success:
@@ -165,7 +178,6 @@ class quantum_volume:
 	def qvolume(self,qlist,sim_results,real_results):
 		QV = 1
 		qv_fitter = QVFitter(qubit_lists=[qlist])
-		# fitter_data = fitter_single(qlist)
 		qv_fitter.add_statevectors(sim_results)
 		qv_fitter.add_data(real_results)
 		qv_success_list = qv_fitter.qv_success()
@@ -177,22 +189,28 @@ class quantum_volume:
 	def q_fitter(self):
 		sim_results=[]
 		real_results=[]
-		qv_circs_real, qv_circs_sim = self.generate_list_of_trails(self._max_depth)
+		if self._qubit_list:
+			qv_circs_real, qv_circs_sim = qv.qv_circuits_opt(self.generate_qubit_lists(), ntrials=self._ntrials,backend=self._backend)     
+		else:
+			qv_circs_real, qv_circs_sim = qv.qv_circuits_opt(ntrials=self._ntrials, max_qubits = self._max_depth,backend=self._backend)
 		for trial in range(self._ntrials):
+			if trial%5==0: print(trial)            
 			sim_results.append(qiskit.execute(qv_circs_sim[trial], backend=self._simulator_backend).result())
-			if self._is_simulator:
+			if self._is_simulator and 'fake' not in self._backend.configuration().backend_name:
 				real_results.append(execute(qv_circs_real[trial], backend = self._backend, noise_model = self._noise_model ,basis_gates=self._basis_gates).result())
+			elif self._is_simulator:
+				real_results.append(execute(qv_circs_real[trial], backend = self._backend,basis_gates=self._basis_gates).result())
 		if not self._is_simulator:
 			all_circs = []
 			for circ in qv_circs_real:
 				all_circs += circ
 			job_manager = IBMQJobManager()
 			managed_job = job_manager.run(all_circs, backend = self._backend)
-		while (managed_job.statuses()[-1] is not JobStatus.DONE):
-			time.sleep(5)
-		real_results = []
-		for ind in range(self._ntrials):
-			real_results.append([])
-			for idx in range(self._max_depth-3):
-				real_results[ind]+=(managed_job.result()._get_result(idx)[0])
+			while (managed_job.statuses()[-1] is not JobStatus.DONE):
+				time.sleep(5)
+			real_results = []
+			for ind in range(self._ntrials):
+				real_results.append([])
+				for idx in range(self._max_depth-3):
+					real_results[ind]+=(managed_job.result()._get_result(idx)[0])
 		return QVFitter(real_results,sim_results,self.generate_qubit_lists())
