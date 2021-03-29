148fd281b9459404e71652aef38ae7424e51e64b
James Wootton <decodoku@gmail.com>
2021-03-29 17:36:47 +0200

    Update qiskit/ignis/verification/topological_codes/fitters.py
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>

--- qiskit/ignis/verification/topological_codes/fitters.py
+++ qiskit/ignis/verification/topological_codes/fitters.py
@@ -181,7 +181,6 @@ class GraphDecoder():
         results = results['0']
         shots = sum(results.values())
 
-        nodes = self.S.nodes()
 
         error_probs = {}
         for edge in self.S.edge_list():
